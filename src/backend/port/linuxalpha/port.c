e63aed333d352ac017c61d3a857b1ec1b40b50ed
Marc G. Fournier <scrappy@postgresql.org>
1997-03-14 06:01:40 +0000

    Last of Dan's hidden tar balls :)

---
+++ src/backend/port/linuxalpha/port.c
@@ -0,0 +1,13 @@
+/*-------------------------------------------------------------------------
+ *
+ * port.c--
+ *    Linux-specific routines
+ *
+ * Copyright (c) 1994, Regents of the University of California
+ *
+ *
+ * IDENTIFICATION
+ *    $Header: /home/cvsmirror/pg/pgsql/src/backend/port/linuxalpha/Attic/port.c,v 1.1 1997/03/14 06:01:40 scrappy Exp $
+ *
+ *-------------------------------------------------------------------------
+ */
