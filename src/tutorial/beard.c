17740ae38f0315131747fb35aeef08a6788b813e
Marc G. Fournier <scrappy@postgresql.org>
1998-03-01 04:51:16 +0000

    Move around files before applying Darren's second patch

--- src/tutorial/beard.c
+++ src/tutorial/beard.c
@@ -7,7 +7,7 @@
  *
  *
  * IDENTIFICATION
- *	  $Header: /home/cvsmirror/pg/pgsql/src/tutorial/C-code/Attic/beard.c,v 1.5 1998/01/07 21:07:03 momjian Exp $
+ *	  $Header: /home/cvsmirror/pg/pgsql/src/tutorial/Attic/beard.c,v 1.1 1998/03/01 04:51:01 scrappy Exp $
  *
  *-------------------------------------------------------------------------
  */
