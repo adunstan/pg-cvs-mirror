20c815528c41165446305e9763b7849f7eaacf04
Bruce Momjian <momjian@postgresql.org>
2001-11-12 02:00:18 +0000

    Add comments and remove CVS tag from md5.c so they remain identical.

--- src/backend/libpq/md5.c
+++ src/backend/libpq/md5.c
@@ -10,7 +10,7 @@
  *
  *	Sverre H. Huseby <sverrehu@online.no>
  *
- * $Header: /home/cvsmirror/pg/pgsql/src/backend/libpq/md5.c,v 1.10 2001/11/12 01:42:03 momjian Exp $
+ * $Header: /home/cvsmirror/pg/pgsql/src/backend/libpq/md5.c,v 1.11 2001/11/12 02:00:18 momjian Exp $
  */
 
 
