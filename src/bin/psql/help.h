e5fe35d060b72b45ddc5b58b8d84513c8849fb74
CVS repo conversion <flames@example.net>
2002-10-23 19:23:57 +0000

    Add files from parent branch HEAD: 	src/bin/psql/command.c 	src/bin/psql/help.h

---
+++ src/bin/psql/help.h
@@ -0,0 +1,19 @@
+/*
+ * psql - the PostgreSQL interactive terminal
+ *
+ * Copyright 2000 by PostgreSQL Global Development Group
+ *
+ * $Header: /home/cvsmirror/pg/pgsql/src/bin/psql/help.h,v 1.10 2002/10/23 19:23:57 momjian Exp $
+ */
+#ifndef HELP_H
+#define HELP_H
+
+void		usage(void);
+
+void		slashUsage(bool pager);
+
+void		helpSQL(const char *topic, bool pager);
+
+void		print_copyright(void);
+
+#endif
