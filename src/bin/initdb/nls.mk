3174f60f02aaa05c30ef0d29184df225f3b464e2
Peter Eisentraut <petere@postgresql.org>
2003-12-11 22:03:05 +0000

    New translation

--- src/bin/initdb/nls.mk
+++ src/bin/initdb/nls.mk
@@ -1,5 +1,5 @@
-# $PostgreSQL: /cvsroot/pgsql-server/src/bin/initdb/nls.mk,v 1.2 2003/11/25 19:19:20 petere Exp $
+# $PostgreSQL: pgsql-server/src/bin/initdb/nls.mk,v 1.3 2003/11/29 19:52:04 pgsql Exp $
 CATALOG_NAME	:= initdb
-AVAIL_LANGUAGES	:= de pt_BR
+AVAIL_LANGUAGES	:= de it pt_BR
 GETTEXT_FILES	:= initdb.c
 GETTEXT_TRIGGERS:= _ simple_prompt
