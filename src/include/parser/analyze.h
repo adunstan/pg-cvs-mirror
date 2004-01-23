f66238a7606da6564c950752f36217b6d3827929
Neil Conway <neilc@postgresql.org>
2004-01-23 02:13:12 +0000

    Fix a minor bug introduced by the recent CREATE TABLE AS / WITH OIDS patch: a 3-value enum was mistakenly assigned directly to a 'bool' in transformCreateStmt(). Along the way, change makeObjectName() to be static, as it isn't used outside analyze.c

--- src/include/parser/analyze.h
+++ src/include/parser/analyze.h
@@ -6,7 +6,7 @@
  * Portions Copyright (c) 1996-2003, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: analyze.h,v 1.23 2003/08/04 02:40:14 momjian Exp $
+ * $PostgreSQL: pgsql-server/src/include/parser/analyze.h,v 1.24 2003/11/29 22:41:09 pgsql Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -23,8 +23,5 @@ extern List *parse_sub_analyze(Node *parseTree, ParseState *parentParseState);
 extern List *analyzeCreateSchemaStmt(CreateSchemaStmt *stmt);
 
 extern void CheckSelectForUpdate(Query *qry);
-
-/* This was exported to allow ADD CONSTRAINT to make use of it */
-extern char *makeObjectName(char *name1, char *name2, char *typename);
 
 #endif   /* ANALYZE_H */
