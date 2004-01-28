5f9f2095ee33901fa3ba8b73f21e456f1a06618d
Michael Meskes <meskes@postgresql.org>
2004-01-28 09:52:14 +0000

    - Issue a warning if a cursor is declared but not opened. - Fixed prototype for ECPGprepared_statement to not moan about "const char" - Fixed parsing of nested structures. - Added option to parse header files.

--- src/interfaces/ecpg/ecpglib/prepare.c
+++ src/interfaces/ecpg/ecpglib/prepare.c
@@ -1,4 +1,4 @@
-/* $PostgreSQL: /cvsroot/pgsql-server/src/interfaces/ecpg/ecpglib/prepare.c,v 1.9 2003/08/04 00:43:32 momjian Exp $ */
+/* $PostgreSQL: pgsql-server/src/interfaces/ecpg/ecpglib/prepare.c,v 1.10 2003/11/29 19:52:08 pgsql Exp $ */
 
 #define POSTGRES_ECPG_INTERNAL
 #include "postgres_fe.h"
@@ -169,7 +169,7 @@ ECPGdeallocate_all(int lineno)
 
 /* return the prepared statement */
 char *
-ECPGprepared_statement(char *name)
+ECPGprepared_statement(const char *name)
 {
 	struct prepared_statement *this;
 
