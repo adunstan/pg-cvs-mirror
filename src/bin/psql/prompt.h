1a4db5944b142e13615a28a430a4b18ccc2ff26e
Tom Lane <tgl@postgresql.org>
2004-02-24 21:45:18 +0000

    Implement dollar-quoting in the backend lexer and psql.  Documentation is still lacking, as is support in plpgsql and other places, but this is the basic feature.  Patch by Andrew Dunstan, some tweaking by Tom Lane. Also, enable %option nodefault in these two lexers, and patch some gaps revealed thereby.

--- src/bin/psql/prompt.h
+++ src/bin/psql/prompt.h
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 2000-2003, PostgreSQL Global Development Group
  *
- * $PostgreSQL: /cvsroot/pgsql-server/src/bin/psql/prompt.h,v 1.12 2003/08/04 23:59:40 tgl Exp $
+ * $PostgreSQL: pgsql-server/src/bin/psql/prompt.h,v 1.13 2003/11/29 19:52:07 pgsql Exp $
  */
 #ifndef PROMPT_H
 #define PROMPT_H
@@ -15,6 +15,7 @@ typedef enum _promptStatus
 	PROMPT_COMMENT,
 	PROMPT_SINGLEQUOTE,
 	PROMPT_DOUBLEQUOTE,
+	PROMPT_DOLLARQUOTE,
 	PROMPT_PAREN,
 	PROMPT_COPY
 } promptStatus_t;
