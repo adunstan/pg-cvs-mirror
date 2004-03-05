2360d1dfc0bfff348ac81c7015b60b03182fbe7c
Bruce Momjian <momjian@postgresql.org>
2004-03-05 02:41:14 +0000

    Per a brief conversation with Tom, I've created a patch for adding support for 'week' within the date_trunc function.
    Within the patch I added a couple of test cases and associated target
    output, and changed the documentation to add 'week' appropriately.
    
    Robert Creager

--- src/backend/utils/adt/timestamp.c
+++ src/backend/utils/adt/timestamp.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql-server/src/backend/utils/adt/timestamp.c,v 1.98 2003/12/25 03:36:23 momjian Exp $
+ *	  $PostgreSQL: pgsql-server/src/backend/utils/adt/timestamp.c,v 1.99 2004/02/14 20:16:17 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -2564,6 +2564,13 @@ timestamp_trunc(PG_FUNCTION_ARGS)
 
 		switch (val)
 		{
+			case DTK_WEEK:
+				isoweek2date( date2isoweek( tm->tm_year, tm->tm_mon, tm->tm_mday ), &(tm->tm_year), &(tm->tm_mon), &(tm->tm_mday) );
+				tm->tm_hour = 0;
+				tm->tm_min = 0;
+				tm->tm_sec = 0;
+				fsec = 0;
+				break;
 			case DTK_MILLENNIUM:
 				tm->tm_year = (tm->tm_year / 1000) * 1000;
 			case DTK_CENTURY:
@@ -2672,6 +2679,13 @@ timestamptz_trunc(PG_FUNCTION_ARGS)
 
 		switch (val)
 		{
+			case DTK_WEEK:
+				isoweek2date( date2isoweek( tm->tm_year, tm->tm_mon, tm->tm_mday ), &(tm->tm_year), &(tm->tm_mon), &(tm->tm_mday) );
+				tm->tm_hour = 0;
+				tm->tm_min = 0;
+				tm->tm_sec = 0;
+				fsec = 0;
+				break;
 			case DTK_MILLENNIUM:
 				tm->tm_year = (tm->tm_year / 1000) * 1000;
 			case DTK_CENTURY:
