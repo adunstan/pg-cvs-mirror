9cf375cac7cc4996195e46e8f2f82226847fcce9
Bruce Momjian <momjian@postgresql.org>
2004-03-15 15:56:28 +0000

    Remove GUC log_statement, log_pid, log_timestamp, log_source_port. Functionality superceeded by log_line_prefix.
    Andrew Dunstan

--- src/backend/tcop/postgres.c
+++ src/backend/tcop/postgres.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql-server/src/backend/tcop/postgres.c,v 1.393 2004/02/21 06:29:58 tgl Exp $
+ *	  $PostgreSQL: pgsql-server/src/backend/tcop/postgres.c,v 1.394 2004/03/09 04:43:07 momjian Exp $
  *
  * NOTES
  *	  this is the "main" module of the postgres backend and
@@ -3212,11 +3212,7 @@ log_disconnections(int code, Datum arg)
 	snprintf(dbname, sizeof(dbname)," database=%s",port->database_name);
 	snprintf(remote_host,sizeof(remote_host)," host=%s",
 			 port->remote_host);
-	/* prevent redundant or empty reporting of port */
-	if (!LogSourcePort && strlen(port->remote_port))
-		snprintf(remote_port,sizeof(remote_port)," port=%s",port->remote_port);
-	else
-		remote_port[0] = '\0';
+	snprintf(remote_port,sizeof(remote_port)," port=%s",port->remote_port);
 
 
 	gettimeofday(&end,NULL);
--- src/include/tcop/tcopprot.h
+++ src/include/tcop/tcopprot.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2003, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: tcopprot.h,v 1.60 2003/08/04 02:40:15 momjian Exp $
+ * $PostgreSQL: pgsql-server/src/include/tcop/tcopprot.h,v 1.61 2003/11/29 22:41:14 pgsql Exp $
  *
  * OLD COMMENTS
  *	  This file was created so that other c files could get the two
@@ -30,7 +30,6 @@ extern bool Warn_restart_ready;
 extern bool InError;
 extern CommandDest whereToSendOutput;
 extern bool log_hostname;
-extern bool LogSourcePort;
 extern DLLIMPORT const char *debug_query_string;
 extern char *rendezvous_name;
 
