8b0f556092bcdda8ac4b82af68e058b70cf4ba69
Tom Lane <tgl@postgresql.org>
2004-03-17 01:02:24 +0000

    Replace the switching function ExecEvalExpr() with a macro that jumps directly to the appropriate per-node execution function, using a function pointer stored by ExecInitExpr.  This speeds things up by eliminating one level of function call.  The function-pointer technique also enables further small improvements such as only making one-time tests once (and then changing the function pointer).  Overall this seems to gain about 10% on evaluation of simple expressions, which isn't earthshaking but seems a worthwhile gain for a relatively small hack.  Per recent discussion on pghackers.

--- src/include/executor/nodeSubplan.h
+++ src/include/executor/nodeSubplan.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2003, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: nodeSubplan.h,v 1.18 2003/08/08 21:42:45 momjian Exp $
+ * $PostgreSQL: pgsql-server/src/include/executor/nodeSubplan.h,v 1.19 2003/11/29 22:41:01 pgsql Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -19,7 +19,8 @@
 extern void ExecInitSubPlan(SubPlanState *node, EState *estate);
 extern Datum ExecSubPlan(SubPlanState *node,
 			ExprContext *econtext,
-			bool *isNull);
+			bool *isNull,
+			ExprDoneCond *isDone);
 extern void ExecEndSubPlan(SubPlanState *node);
 extern void ExecReScanSetParamPlan(SubPlanState *node, PlanState *parent);
 
