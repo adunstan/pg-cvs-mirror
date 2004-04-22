708220e2afecca46145225ee396bfa0329204ee2
Bruce Momjian <momjian@postgresql.org>
2004-04-22 03:48:38 +0000

    Attached is are a few small fixes for dbmirror.
    1. Fixed bug where sequences were being mirrored incorrectly if they
    exceeded 127
    2. Fixed a bug in the perl script with mirroring sequences(John
    Burtenshaw sent an email to patches describing the bug in March but I
    htink he forgot to attach his patch)
    3. The dates/times in the transaction files will always use 2 digits.
    
    Steven Singer

--- contrib/dbmirror/pending.c
+++ contrib/dbmirror/pending.c
@@ -1,7 +1,7 @@
 /****************************************************************************
  * pending.c
- * $Id: pending.c,v 1.16 2004/02/17 03:34:35 momjian Exp $
- * $PostgreSQL: pgsql-server/contrib/dbmirror/pending.c,v 1.15 2003/11/29 22:39:19 pgsql Exp $
+ * $Id: pending.c,v 1.17 2004/04/22 03:48:38 momjian Exp $
+ * $PostgreSQL: pgsql-server/contrib/dbmirror/pending.c,v 1.16 2004/02/17 03:34:35 momjian Exp $
  *
  * This file contains a trigger for Postgresql-7.x to record changes to tables
  * to a pending table for mirroring.
@@ -701,7 +701,7 @@ nextval(PG_FUNCTION_ARGS)
 
   debug_msg("dbmirror:nextval Set resTuple");
 
-  nextSequenceValue =*(DatumGetPointer(SPI_getbinval(resTuple,
+  nextSequenceValue =* (unsigned int *)(DatumGetPointer(SPI_getbinval(resTuple,
 						     SPI_tuptable->tupdesc,
 						     1,&isNull)));
 
