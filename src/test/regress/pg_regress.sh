833a6dd078aba35cd65f0636c60bf8da563d58ec
Bruce Momjian <momjian@postgresql.org>
2004-05-02 21:30:08 +0000

    Use $build_platform for PWD flag test, per suggestion from Peter.

--- src/test/regress/pg_regress.sh
+++ src/test/regress/pg_regress.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-# $PostgreSQL: pgsql-server/src/test/regress/pg_regress.sh,v 1.39 2004/05/02 04:27:37 momjian Exp $
+# $PostgreSQL: pgsql-server/src/test/regress/pg_regress.sh,v 1.40 2004/05/02 10:24:51 momjian Exp $
 
 me=`basename $0`
 : ${TMPDIR=/tmp}
@@ -211,8 +211,8 @@ esac
 # Set up pwd to give a win32 happy pathname
 # ----------
 
-case `uname` in
-     MINGW32*)
+case $build_platform in
+    *-*-mingw32*)
         PWDFLAGS=-W;;
     *)
         PWDFLAGS=;;
