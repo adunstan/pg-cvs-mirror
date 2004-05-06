4d8608e8a05e2019762cbb846151bc76fc94f439
Neil Conway <neilc@postgresql.org>
2004-05-06 06:11:01 +0000

    Fix grammatical error in comment.

--- src/backend/nodes/nodes.c
+++ src/backend/nodes/nodes.c
@@ -1,15 +1,15 @@
 /*-------------------------------------------------------------------------
  *
  * nodes.c
- *	  support code for nodes (now that we get rid of the home-brew
- *	  inheritance system, our support code for nodes get much simpler)
+ *	  support code for nodes (now that we have removed the home-brew
+ *	  inheritance system, our support code for nodes is much simpler)
  *
  * Portions Copyright (c) 1996-2003, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: /cvsroot/pgsql-server/src/backend/nodes/nodes.c,v 1.21 2003/08/04 02:39:59 momjian Exp $
+ *	  $PostgreSQL: pgsql-server/src/backend/nodes/nodes.c,v 1.22 2003/11/29 19:51:49 pgsql Exp $
  *
  * HISTORY
  *	  Andrew Yu			Oct 20, 1994	file creation
