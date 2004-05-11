ff80d52d7f32069b0e8a8c9d47bf58a4e9b387c8
Tom Lane <tgl@postgresql.org>
2004-05-11 13:15:15 +0000

    Fix oversights in processing of LIMIT expressions during planning.

--- src/backend/optimizer/plan/setrefs.c
+++ src/backend/optimizer/plan/setrefs.c
@@ -9,7 +9,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql-server/src/backend/optimizer/plan/setrefs.c,v 1.99 2003/11/29 19:51:50 pgsql Exp $
+ *	  $PostgreSQL: pgsql-server/src/backend/optimizer/plan/setrefs.c,v 1.100 2004/01/06 04:31:01 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -171,7 +171,6 @@ set_plan_references(Plan *plan, List *rtable)
 		case T_Sort:
 		case T_Unique:
 		case T_SetOp:
-		case T_Limit:
 
 			/*
 			 * These plan types don't actually bother to evaluate their
@@ -183,6 +182,15 @@ set_plan_references(Plan *plan, List *rtable)
 			 * reprocessing subplans that also appear in lower levels of
 			 * the plan tree!
 			 */
+			break;
+		case T_Limit:
+			/*
+			 * Like the plan types above, Limit doesn't evaluate its
+			 * tlist or quals.  It does have live expressions for
+			 * limit/offset, however.
+			 */
+			fix_expr_references(plan, ((Limit *) plan)->limitOffset);
+			fix_expr_references(plan, ((Limit *) plan)->limitCount);
 			break;
 		case T_Agg:
 		case T_Group:
