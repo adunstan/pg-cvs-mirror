f5d412368384b0c4dd2d2f0a1a7677e34d175f65
Bruce Momjian <momjian@postgresql.org>
2004-05-21 12:30:25 +0000

    Pgindent timezone file, per request from Tom.

--- src/timezone/pgtz.h
+++ src/timezone/pgtz.h
@@ -9,7 +9,7 @@
  * Portions Copyright (c) 1996-2003, PostgreSQL Global Development Group
  *
  * IDENTIFICATION
- *	  $PostgreSQL$
+ *	  $PostgreSQL: pgsql-server/src/timezone/pgtz.h,v 1.7 2004/05/21 05:08:06 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -18,6 +18,6 @@
 
 #define TZ_STRLEN_MAX 255
 
-extern char	   *pg_TZDIR(void);
+extern char *pg_TZDIR(void);
 
-#endif /* _PGTZ_H */
+#endif   /* _PGTZ_H */
