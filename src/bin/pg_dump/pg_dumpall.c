7aae872629b252eafc0098bebe313208a42776d9
Bruce Momjian <momjian@postgresql.org>
2004-06-09 17:05:14 +0000

    Add time.h to pg_dumpall.

--- src/bin/pg_dump/pg_dumpall.c
+++ src/bin/pg_dump/pg_dumpall.c
@@ -6,7 +6,7 @@
  * Portions Copyright (c) 1994, Regents of the University of California
  *
  *
- * $PostgreSQL: pgsql-server/src/bin/pg_dump/pg_dumpall.c,v 1.37 2004/06/05 04:27:48 momjian Exp $
+ * $PostgreSQL: pgsql-server/src/bin/pg_dump/pg_dumpall.c,v 1.38 2004/06/07 20:35:57 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -21,6 +21,7 @@
 #include "strdup.h"
 #endif
 #include <errno.h>
+#include <time.h>
 
 #include "getopt_long.h"
 
