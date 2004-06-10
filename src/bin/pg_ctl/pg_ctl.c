a0036ec3b8be9dc02049c059530d38c888e22d01
Bruce Momjian <momjian@postgresql.org>
2004-06-10 17:26:14 +0000

    Use START for pg_ctl background runs on Win32, from Magnus.

--- src/bin/pg_ctl/pg_ctl.c
+++ src/bin/pg_ctl/pg_ctl.c
@@ -4,7 +4,7 @@
  *
  * Portions Copyright (c) 1996-2003, PostgreSQL Global Development Group
  *
- * $PostgreSQL: pgsql-server/src/bin/pg_ctl/pg_ctl.c,v 1.8 2004/06/09 17:36:07 momjian Exp $
+ * $PostgreSQL: pgsql-server/src/bin/pg_ctl/pg_ctl.c,v 1.9 2004/06/10 16:35:17 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -222,9 +222,13 @@ start_postmaster(void)
 	 */
 	char		cmd[MAXPGPATH];
 
-	/* Does '&' work on Win32? */
 	if (log_file != NULL)
+		/* Win32 needs START rather than "&" */
+#ifndef WIN32
 		snprintf(cmd, MAXPGPATH, "%s\"%s\" %s < %s >> \"%s\" 2>&1 &%s",
+#else
+		snprintf(cmd, MAXPGPATH, "START %s\"%s\" %s < %s >> \"%s\" 2>&1%s",
+#endif
 				 SYSTEMQUOTE, postgres_path, post_opts, DEVNULL, log_file,
 				 SYSTEMQUOTE);
 	else
