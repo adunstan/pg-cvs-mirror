299f7a3a6a30a4537ed8735e18cc3fc9f320051c
Bruce Momjian <momjian@postgresql.org>
2004-06-19 04:22:17 +0000

    Enable thread safety for win32.mak build of PostgreSQL.
    Andreas Pflug

--- src/interfaces/libpq/fe-connect.c
+++ src/interfaces/libpq/fe-connect.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql-server/src/interfaces/libpq/fe-connect.c,v 1.273 2004/06/08 13:49:23 momjian Exp $
+ *	  $PostgreSQL: pgsql-server/src/interfaces/libpq/fe-connect.c,v 1.274 2004/06/10 22:26:24 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -882,10 +882,12 @@ connectDBStart(PGconn *conn)
 	const char *node = NULL;
 	int			ret;
 #ifdef ENABLE_THREAD_SAFETY
+#ifndef WIN32
 	static pthread_once_t check_sigpipe_once = PTHREAD_ONCE_INIT;
 
 	/* Check only on first connection request */
 	pthread_once(&check_sigpipe_once, check_sigpipe_handler);
+#endif
 #endif
 
 	if (!conn)
@@ -3183,11 +3185,19 @@ PQinitSSL(int do_init)
 }
 
 static pgthreadlock_t default_threadlock;
+
 static void
 default_threadlock(int acquire)
 {
 #ifdef ENABLE_THREAD_SAFETY
+#ifndef WIN32
 	static pthread_mutex_t singlethread_lock = PTHREAD_MUTEX_INITIALIZER;
+#else
+	static pthread_mutex_t singlethread_lock;
+        static long mutex_initialized = 0;
+        if (!InterlockedExchange(&mutex_initialized, 1L))
+                pthread_mutex_init(&singlethread_lock, NULL);
+#endif
 	if (acquire)
 		pthread_mutex_lock(&singlethread_lock);
 	else
