c46c2a0ce99931319bdd317938ce73b51957972b
Tom Lane <tgl@postgresql.org>
2004-07-02 22:50:23 +0000

    Add missing operators of the form interval-plus-datetime, as required for better SQL compliance in this area, per recent discussion.  Mark related operators as commutators where possible.  (The system doesn't actually care about commutator marking for operators not returning boolean, at the moment, but this seems forward-thinking and besides it made it easier to verify that we hadn't missed any.) Also, remove interval-minus-time and interval-minus-timetz operators. I'm not sure how these got in, but they are nonstandard and had very obviously broken behavior.  (minus is not commutative in anyone's book.) I doubt anyone had ever used 'em, because we'd surely have gotten a bug report about it if so.

--- src/include/utils/date.h
+++ src/include/utils/date.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2003, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql-server/src/include/utils/date.h,v 1.25 2003/11/29 22:41:15 pgsql Exp $
+ * $PostgreSQL: pgsql-server/src/include/utils/date.h,v 1.26 2004/02/14 20:16:18 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -147,7 +147,6 @@ extern Datum text_time(PG_FUNCTION_ARGS);
 extern Datum time_text(PG_FUNCTION_ARGS);
 extern Datum time_pl_interval(PG_FUNCTION_ARGS);
 extern Datum time_mi_interval(PG_FUNCTION_ARGS);
-extern Datum interval_pl_time(PG_FUNCTION_ARGS);
 extern Datum time_part(PG_FUNCTION_ARGS);
 
 extern Datum timetz_in(PG_FUNCTION_ARGS);
