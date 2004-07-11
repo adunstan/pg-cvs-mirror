7fab2ae519c6e224cbd9da4f67e1a35cf021d718
Tom Lane <tgl@postgresql.org>
2004-07-11 19:52:52 +0000

    Move TablespaceCreateDbspace() call into smgrcreate(), which is where it probably should have been to begin with; this is to cover cases like needing to recreate the per-db directory during WAL replay. Also, fix heap_create to force pg_class.reltablespace to be zero instead of the database's default tablespace; this makes the world safe for CREATE DATABASE to handle all tables in the default tablespace alike, as per previous discussion.  And force pg_class.reltablespace to zero when creating a relation without physical storage (eg, a view); this avoids possibly having dangling references in this column after a subsequent DROP TABLESPACE.

--- src/include/catalog/heap.h
+++ src/include/catalog/heap.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2003, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql-server/src/include/catalog/heap.h,v 1.66 2004/05/05 04:48:47 tgl Exp $
+ * $PostgreSQL: pgsql-server/src/include/catalog/heap.h,v 1.67 2004/06/18 06:14:05 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -40,10 +40,8 @@ extern Relation heap_create(const char *relname,
 			Oid reltablespace,
 			TupleDesc tupDesc,
 			bool shared_relation,
-			bool storage_create,
+			bool create_storage,
 			bool allow_system_table_mods);
-
-extern void heap_storage_create(Relation rel);
 
 extern Oid heap_create_with_catalog(const char *relname,
 						 Oid relnamespace,
