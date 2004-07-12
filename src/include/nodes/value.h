54820eae2fffbc0cfe14e7afd71953fc131ab961
Bruce Momjian <momjian@postgresql.org>
2004-07-12 01:00:45 +0000

    The outer #define was forgotten.  Attached patch adds it; please apply.
    Alvaro

--- src/include/nodes/value.h
+++ src/include/nodes/value.h
@@ -6,10 +6,13 @@
  *
  * Copyright (c) 2003, PostgreSQL Global Development Group
  *
- * $PostgreSQL$
+ * $PostgreSQL: pgsql-server/src/include/nodes/value.h,v 1.1 2004/01/07 18:43:36 neilc Exp $
  *
  *-------------------------------------------------------------------------
  */
+
+#ifndef VALUE_H
+#define VALUE_H
 
 #include "nodes/nodes.h"
 
@@ -54,3 +57,5 @@ extern Value *makeInteger(long i);
 extern Value *makeFloat(char *numericStr);
 extern Value *makeString(char *str);
 extern Value *makeBitString(char *str);
+
+#endif	/* VALUE_H */
