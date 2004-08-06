acdf0519727d9376e474855387f8d4c1c730696a
Tom Lane <tgl@postgresql.org>
2004-08-06 18:05:49 +0000

    Use one, not zero, as the default lower bound for arrays of AclItems. This avoids changing the displayed appearance of ACL columns now that array_out decorates its output with bounds information when the lower bound isn't one.  Per gripe from Gaetano Mendola.  Note that I did not force initdb for this, although any database initdb'd in the last couple of days is going to have some problems.

--- src/backend/utils/adt/acl.c
+++ src/backend/utils/adt/acl.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql-server/src/backend/utils/adt/acl.c,v 1.107 2004/07/12 20:23:50 momjian Exp $
+ *	  $PostgreSQL: pgsql-server/src/backend/utils/adt/acl.c,v 1.108 2004/08/01 20:30:49 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -328,7 +328,7 @@ allocacl(int n)
 	new_acl->ndim = 1;
 	new_acl->flags = 0;
 	new_acl->elemtype = ACLITEMOID;
-	ARR_LBOUND(new_acl)[0] = 0;
+	ARR_LBOUND(new_acl)[0] = 1;
 	ARR_DIMS(new_acl)[0] = n;
 	return new_acl;
 }
