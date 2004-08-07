7bf8cdd8af670add760d7631f10abf6956e6b2d1
Tom Lane <tgl@postgresql.org>
2004-08-07 17:40:49 +0000

    Don't try to rewrite NEW references in a utility statement in a rule. There won't be any, and in fact there won't even be an RTE for NEW, which was leading to a core dump in CVS tip.  7.4 and earlier manage not to crash when applying ResolveNew in this scenario, but I think it was just good fortune that they didn't.  Per report from Bernd Helmle.

--- src/backend/rewrite/rewriteHandler.c
+++ src/backend/rewrite/rewriteHandler.c
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1994, Regents of the University of California
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql-server/src/backend/rewrite/rewriteHandler.c,v 1.139 2004/06/09 19:08:17 tgl Exp $
+ *	  $PostgreSQL: pgsql-server/src/backend/rewrite/rewriteHandler.c,v 1.140 2004/06/16 01:26:46 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -208,7 +208,8 @@ rewriteRuleAction(Query *parsetree,
 	 * apply it to sub_action; we have to remember to update the sublink
 	 * inside rule_action, too.
 	 */
-	if (event == CMD_INSERT || event == CMD_UPDATE)
+	if ((event == CMD_INSERT || event == CMD_UPDATE) &&
+		sub_action->commandType != CMD_UTILITY)
 	{
 		sub_action = (Query *) ResolveNew((Node *) sub_action,
 										  new_varno,
