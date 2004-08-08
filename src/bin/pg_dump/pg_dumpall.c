acd236d8153428d9f16d9f473d6cc2bb225fde2c
Bruce Momjian <momjian@postgresql.org>
2004-08-08 06:58:00 +0000

    Supress warning about zero-length format string.

--- src/bin/pg_dump/pg_dumpall.c
+++ src/bin/pg_dump/pg_dumpall.c
@@ -6,7 +6,7 @@
  * Portions Copyright (c) 1994, Regents of the University of California
  *
  *
- * $PostgreSQL: pgsql-server/src/bin/pg_dump/pg_dumpall.c,v 1.46 2004/08/04 21:34:12 tgl Exp $
+ * $PostgreSQL: pgsql-server/src/bin/pg_dump/pg_dumpall.c,v 1.47 2004/08/08 06:44:33 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -858,9 +858,11 @@ runPgDump(const char *dbname)
 #ifndef WIN32
 		if (*p == '\'')
 			appendPQExpBuffer(cmd, "'\"'\"'");
-		else
+#else
+		if (*p == '"')
+			appendPQExpBuffer(cmd, "\\\"");
 #endif
-		/* not needed on Win32 */
+		else
 			appendPQExpBufferChar(cmd, *p);
 	}
 
@@ -868,10 +870,8 @@ runPgDump(const char *dbname)
 	appendPQExpBufferChar(cmd, '\'');
 #else
 	appendPQExpBufferChar(cmd, '"');
+	appendPQExpBuffer(cmd, SYSTEMQUOTE);
 #endif
-
-	if (strlen(SYSTEMQUOTE) > 0)
-		appendPQExpBuffer(cmd, SYSTEMQUOTE);
 	
 	if (verbose)
 		fprintf(stderr, _("%s: running \"%s\"\n"), progname, cmd->data);
