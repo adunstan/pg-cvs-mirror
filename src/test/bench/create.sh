ee0ae5b712d43e859b43f093399cdbc03b1edf33
Tom Lane <tgl@postgresql.org>
2004-09-01 17:25:40 +0000

    Whack Wisconsin benchmark around until it actually works again. It's still useless because it tests a standalone backend, but at least the bit rot is repaired.

--- src/test/bench/create.sh
+++ src/test/bench/create.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-# $PostgreSQL: /cvsroot/pgsql-server/src/test/bench/create.sh,v 1.2 1997/04/17 13:48:49 scrappy Exp $
+# $PostgreSQL: pgsql-server/src/test/bench/create.sh,v 1.3 2003/11/29 19:52:14 pgsql Exp $
 # 
 if [ ! -d $1 ]; then
-	echo " you must specify a valid data directory "
+	echo " you must specify a valid data directory " >&2
 	exit
 fi
 if [ -d ./obj ]; then
@@ -19,7 +19,7 @@ if [ $? -ne 0 ]; then
 	exit 1
 fi
 
-postgres -D${1} -Q bench < create.sql > /dev/null
+postgres -D${1} bench < create.sql > /dev/null
 if [ $? -ne 0 ]; then
 	echo initial database load failed
 	exit 1
--- src/test/bench/runwisc.sh
+++ src/test/bench/runwisc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $PostgreSQL: /cvsroot/pgsql-server/src/test/bench/runwisc.sh,v 1.4 1999/05/23 18:53:30 momjian Exp $
+# $PostgreSQL: pgsql-server/src/test/bench/runwisc.sh,v 1.5 2003/11/29 19:52:14 pgsql Exp $
 # 
 # Note that in our published benchmark numbers, we executed the command in the
 # following fashion:
@@ -7,7 +7,7 @@
 # time $POSTGRES -texecutor -tplanner -f hashjoin -Q bench
 #
 if [ ! -d $1 ]; then
-        echo " you must specify a valid data directory "
+        echo " you must specify a valid data directory " >&2
         exit
 fi
 
@@ -15,8 +15,8 @@ if [ -d ./obj ]; then
 	cd ./obj
 fi
 
-echo =============== vacuuming benchmark database... =================
-echo "vacuum" | postgres -D${1} -Q bench > /dev/null
+echo =============== vacuuming benchmark database... ================= >&2
+echo "vacuum" | postgres -D${1} bench > /dev/null
 
-echo =============== running benchmark... =================
-time postgres -D${1} -texecutor -tplanner -Q bench < bench.sql
+echo =============== running benchmark... ================= >&2
+time postgres -D${1} -texecutor -tplanner -c log_min_messages=log -c log_destination=stderr -c redirect_stderr=off bench < bench.sql 2>&1
