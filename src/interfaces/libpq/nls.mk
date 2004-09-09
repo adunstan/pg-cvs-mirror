740d5628be88635a693a39f931b58cdb48f0f187
dennis <dennis>
2004-09-09 06:32:13 +0000

    Some source files was not scanned for translatable strings.

--- src/interfaces/libpq/nls.mk
+++ src/interfaces/libpq/nls.mk
@@ -1,5 +1,5 @@
-# $PostgreSQL: pgsql-server/src/interfaces/libpq/nls.mk,v 1.15 2004/02/15 19:38:28 petere Exp $
+# $PostgreSQL: pgsql-server/src/interfaces/libpq/nls.mk,v 1.16 2004/08/02 15:17:21 petere Exp $
 CATALOG_NAME	:= libpq
 AVAIL_LANGUAGES	:= af cs de es fr hr it nb pt_BR ru sk sl sv zh_CN zh_TW
-GETTEXT_FILES	:= fe-auth.c fe-connect.c fe-exec.c fe-lobj.c fe-misc.c fe-secure.c
+GETTEXT_FILES	:= fe-auth.c fe-connect.c fe-exec.c fe-lobj.c fe-misc.c fe-protocol2.c fe-protocol3.c fe-secure.c
 GETTEXT_TRIGGERS:= libpq_gettext pqInternalNotice:2
