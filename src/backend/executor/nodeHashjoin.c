fade0d3942f9612209569d34b18ed32af99278e6
Tom Lane <tgl@postgresql.org>
2004-09-22 19:13:52 +0000

    Arrange for hash join to skip scanning the outer relation if it detects that the inner one is completely empty.  Per recent discussion.  Also some cosmetic cleanups in nearby code.

--- src/backend/executor/nodeHashjoin.c
+++ src/backend/executor/nodeHashjoin.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql-server/src/backend/executor/nodeHashjoin.c,v 1.64 2004/08/29 05:06:42 momjian Exp $
+ *	  $PostgreSQL: pgsql-server/src/backend/executor/nodeHashjoin.c,v 1.65 2004/09/17 18:28:53 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -128,6 +128,17 @@ ExecHashJoin(HashJoinState *node)
 		(void) ExecProcNode((PlanState *) hashNode);
 
 		/*
+		 * If the inner relation is completely empty, and we're not doing
+		 * an outer join, we can quit without scanning the outer relation.
+		 */
+		if (!hashtable->hashNonEmpty && node->js.jointype != JOIN_LEFT)
+		{
+			ExecHashTableDestroy(hashtable);
+			node->hj_HashTable = NULL;
+			return NULL;
+		}
+
+		/*
 		 * Open temp files for outer batches, if needed. Note that file
 		 * buffers are palloc'd in regular executor context.
 		 */
@@ -138,10 +149,8 @@ ExecHashJoin(HashJoinState *node)
 	}
 
 	/*
-	 * Now get an outer tuple and probe into the hash table for matches
+	 * run the hash join process
 	 */
-	outerTupleSlot = node->js.ps.ps_OuterTupleSlot;
-
 	for (;;)
 	{
 		/*
@@ -226,7 +235,7 @@ ExecHashJoin(HashJoinState *node)
 			 * Only the joinquals determine MatchedOuter status, but all
 			 * quals must pass to actually return the tuple.
 			 */
-			if (ExecQual(joinqual, econtext, false))
+			if (joinqual == NIL || ExecQual(joinqual, econtext, false))
 			{
 				node->hj_MatchedOuter = true;
 
