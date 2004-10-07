c0d8f98042509ac2c4894174676402dd7f029182
Bruce Momjian <momjian@postgresql.org>
2004-10-07 17:29:12 +0000

    Add one more byte to malloc for null storage.

--- src/bin/initdb/initdb.c
+++ src/bin/initdb/initdb.c
@@ -39,7 +39,7 @@
  * Portions Copyright (c) 1994, Regents of the University of California
  * Portions taken from FreeBSD.
  *
- * $PostgreSQL: pgsql-server/src/bin/initdb/initdb.c,v 1.56 2004/10/06 09:13:10 momjian Exp $
+ * $PostgreSQL: pgsql-server/src/bin/initdb/initdb.c,v 1.57 2004/10/07 16:53:25 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -1909,7 +1909,7 @@ escape_locale(char **locale)
 {
 	int			len = strlen(*locale),
 				i, j;
-	char		*loc_temp = xmalloc(len * 2);
+	char		*loc_temp = xmalloc(len * 2 + 1);
 	
 	for (i = 0, j = 0; i < len; i++)
 	{
