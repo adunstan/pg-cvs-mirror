a63e11aad262c8428011728dc983fc760668c5a4
Tom Lane <tgl@postgresql.org>
2004-12-23 22:42:15 +0000

    Avoid memory leakage during VACUUM FULL when an index expression or index predicate uses temporary memory for evaluation.  Per example from Jean-Gerard Pailloncy.

--- src/backend/commands/vacuum.c
+++ src/backend/commands/vacuum.c
@@ -13,7 +13,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/commands/vacuum.c,v 1.296 2004/12/01 19:00:41 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/backend/commands/vacuum.c,v 1.297 2004/12/02 19:28:49 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -2553,6 +2553,7 @@ move_chain_tuple(Relation rel,
 	{
 		ExecStoreTuple(&newtup, ec->slot, InvalidBuffer, false);
 		ExecInsertIndexTuples(ec->slot, &(newtup.t_self), ec->estate, true);
+		ResetPerTupleExprContext(ec->estate);
 	}
 }
 
@@ -2662,6 +2663,7 @@ move_plain_tuple(Relation rel,
 	{
 		ExecStoreTuple(&newtup, ec->slot, InvalidBuffer, false);
 		ExecInsertIndexTuples(ec->slot, &(newtup.t_self), ec->estate, true);
+		ResetPerTupleExprContext(ec->estate);
 	}
 }
 
