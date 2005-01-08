548ffa40752ae2a7a7cd46aa618575a59f8dd553
Tom Lane <tgl@postgresql.org>
2005-01-08 22:51:15 +0000

    Consistently use geteuid() not getuid(); there were a few places deviating from our long-established standard.

--- src/bin/psql/help.c
+++ src/bin/psql/help.c
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 2000-2005, PostgreSQL Global Development Group
  *
- * $PostgreSQL: pgsql/src/bin/psql/help.c,v 1.98 2004/12/31 22:03:15 pgsql Exp $
+ * $PostgreSQL: pgsql/src/bin/psql/help.c,v 1.99 2005/01/01 05:43:08 momjian Exp $
  */
 #include "postgres_fe.h"
 #include "common.h"
@@ -19,7 +19,7 @@
 #include <pwd.h>				/* for getpwuid() */
 #endif
 #include <sys/types.h>			/* (ditto) */
-#include <unistd.h>				/* for getuid() */
+#include <unistd.h>				/* for geteuid() */
 #else
 #include <win32.h>
 #endif
