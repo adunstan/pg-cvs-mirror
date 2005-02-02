704e436918829fea05e188b1435a63f0516ba9f4
Neil Conway <neilc@postgresql.org>
2005-02-02 06:36:02 +0000

    Add support for temporary views, including documentation and regression tests. Contributed by Koju Iijima, review from Neil Conway, Gavin Sherry and Tom Lane.
    Also, fix error in description of WITH CHECK OPTION clause in the CREATE
    VIEW reference page: it should be "CASCADED", not "CASCADE".

--- src/include/commands/view.h
+++ src/include/commands/view.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2005, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/commands/view.h,v 1.20 2004/08/29 04:13:06 momjian Exp $
+ * $PostgreSQL: pgsql/src/include/commands/view.h,v 1.21 2004/12/31 22:03:28 pgsql Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -16,7 +16,7 @@
 
 #include "nodes/parsenodes.h"
 
-extern void DefineView(const RangeVar *view, Query *view_parse, bool replace);
+extern void DefineView(RangeVar *view, Query *view_parse, bool replace);
 extern void RemoveView(const RangeVar *view, DropBehavior behavior);
 
 #endif   /* VIEW_H */
