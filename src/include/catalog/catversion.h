1f0d3197d443387244bf43ae51ca839d1a1d7504
Teodor Sigaev <teodor@postgresql.org>
2005-02-25 16:13:29 +0000

    In accordance to http://www.pgsql.ru/db/mw/msg.html?mid=2045361
    change TimeATD to/from Datum macros.
    
    Re-initdb is needed.

--- src/include/catalog/catversion.h
+++ src/include/catalog/catversion.h
@@ -37,7 +37,7 @@
  * Portions Copyright (c) 1996-2005, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/catalog/catversion.h,v 1.252 2004/11/05 19:16:32 tgl Exp $
+ * $PostgreSQL: pgsql/src/include/catalog/catversion.h,v 1.253 2004/12/31 22:03:24 pgsql Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -53,6 +53,6 @@
  */
 
 /*							yyyymmddN */
-#define CATALOG_VERSION_NO	200411041
+#define CATALOG_VERSION_NO	200502251
 
 #endif
