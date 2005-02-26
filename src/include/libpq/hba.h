1b74c06117fcd17501501469c83d3acf25b96770
Tom Lane <tgl@postgresql.org>
2005-02-26 18:43:34 +0000

    Finish up the flat-files project: get rid of GetRawDatabaseInfo() hack in favor of looking at the flat file copy of pg_database during backend startup.  This should finally eliminate the various corner cases in which backend startup fails unexpectedly because it isn't able to distinguish live and dead tuples in pg_database.  Simplify locking on pg_database to be similar to the rules used with pg_shadow and pg_group, and eliminate FlushRelationBuffers operations that were used only to reduce the odds of failure of GetRawDatabaseInfo. initdb forced due to addition of a trigger to pg_database.

--- src/include/libpq/hba.h
+++ src/include/libpq/hba.h
@@ -4,18 +4,15 @@
  *	  Interface to hba.c
  *
  *
- * $PostgreSQL: pgsql-server/src/include/libpq/hba.h,v 1.34 2003/11/29 22:41:03 pgsql Exp $
+ * $PostgreSQL: pgsql/src/include/libpq/hba.h,v 1.35 2004/02/02 16:58:30 neilc Exp $
  *
  *-------------------------------------------------------------------------
  */
 #ifndef HBA_H
 #define HBA_H
 
-#ifndef WIN32
-#include <netinet/in.h>
-#endif
-
 #include "nodes/pg_list.h"
+
 
 typedef enum UserAuth
 {
@@ -41,5 +38,7 @@ extern void load_user(void);
 extern void load_group(void);
 extern int	hba_getauthmethod(hbaPort *port);
 extern int	authident(hbaPort *port);
+extern bool	read_pg_database_line(FILE *fp, char *dbname,
+								  Oid *dboid, Oid *dbtablespace);
 
-#endif
+#endif /* HBA_H */
