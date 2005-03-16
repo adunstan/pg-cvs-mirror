d46bf272738c84bf95c40efb171a10eb349e1752
Bruce Momjian <momjian@postgresql.org>
2005-03-16 01:49:20 +0000

    Add missing include for new lc_ctype_is_c() function.
    Per Neil.

--- src/backend/utils/adt/oracle_compat.c
+++ src/backend/utils/adt/oracle_compat.c
@@ -9,7 +9,7 @@
  *
  *
  * IDENTIFICATION
- *	$PostgreSQL: pgsql/src/backend/utils/adt/oracle_compat.c,v 1.57 2004/12/31 22:01:22 pgsql Exp $
+ *	$PostgreSQL: pgsql/src/backend/utils/adt/oracle_compat.c,v 1.57.4.1 2005/03/16 00:03:01 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -29,6 +29,7 @@
 #endif
 
 #include "utils/builtins.h"
+#include "utils/pg_locale.h"
 #include "mb/pg_wchar.h"
 
 
