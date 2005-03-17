da04c31affdc0b14e03efcc3bf3768c4cf9316a1
Neil Conway <neilc@postgresql.org>
2005-03-17 23:45:09 +0000

    This patch moves some code for preprocessing FOR UPDATE from grouping_planner() to preprocess_targetlist(), according to a comment in grouping_planner(). I think the refactoring makes sense, and moves some extraneous details out of grouping_planner().

--- src/include/optimizer/prep.h
+++ src/include/optimizer/prep.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2005, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/optimizer/prep.h,v 1.46 2004/10/02 22:39:49 tgl Exp $
+ * $PostgreSQL: pgsql/src/include/optimizer/prep.h,v 1.47 2004/12/31 22:03:36 pgsql Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -42,8 +42,7 @@ extern Node *flatten_andors(Node *node);
 /*
  * prototypes for preptlist.c
  */
-extern List *preprocess_targetlist(List *tlist, int command_type,
-					  Index result_relation, List *range_table);
+extern List *preprocess_targetlist(Query *parse, List *tlist);
 
 /*
  * prototypes for prepunion.c
