f485a3aaf134b0b2d506bc3fb10a915a8e6286b9
Tom Lane <tgl@postgresql.org>
2005-03-19 23:27:11 +0000

    Add temp_buffers GUC variable to allow users to determine the size of the local buffer arena for temporary table access.

--- src/bin/psql/tab-complete.c
+++ src/bin/psql/tab-complete.c
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 2000-2005, PostgreSQL Global Development Group
  *
- * $PostgreSQL: pgsql/src/bin/psql/tab-complete.c,v 1.120 2005/01/01 05:43:08 momjian Exp $
+ * $PostgreSQL: pgsql/src/bin/psql/tab-complete.c,v 1.121 2005/01/23 15:58:50 momjian Exp $
  */
 
 /*----------------------------------------------------------------------
@@ -594,6 +594,7 @@ psql_completion(char *text, int start, int end)
 		"superuser_reserved_connections",
 		"syslog_facility",
 		"syslog_ident",
+		"temp_buffers",
 		"TimeZone",
 		"trace_notify",
 		"transform_null_equals",
