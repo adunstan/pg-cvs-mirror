b0512a4a948ec0ebf74b26a9c4e1e5f10b432d40
Tom Lane <tgl@postgresql.org>
2005-03-25 16:17:39 +0000

    Add missing min/max parameters to DefineCustomIntVariable() and DefineCustomRealVariable().  Thomas Hallgren

--- src/include/utils/guc.h
+++ src/include/utils/guc.h
@@ -7,7 +7,7 @@
  * Copyright (c) 2000-2005, PostgreSQL Global Development Group
  * Written by Peter Eisentraut <peter_e@gmx.net>.
  *
- * $PostgreSQL: pgsql/src/include/utils/guc.h,v 1.57 2004/11/24 19:51:05 tgl Exp $
+ * $PostgreSQL: pgsql/src/include/utils/guc.h,v 1.58 2005/01/01 05:43:09 momjian Exp $
  *--------------------------------------------------------------------
  */
 #ifndef GUC_H
@@ -149,6 +149,8 @@ extern void DefineCustomIntVariable(
 						const char *short_desc,
 						const char *long_desc,
 						int *valueAddr,
+						int minValue,
+						int maxValue,
 						GucContext context,
 						GucIntAssignHook assign_hook,
 						GucShowHook show_hook);
@@ -158,6 +160,8 @@ extern void DefineCustomRealVariable(
 						 const char *short_desc,
 						 const char *long_desc,
 						 double *valueAddr,
+						 double minValue,
+						 double maxValue,
 						 GucContext context,
 						 GucRealAssignHook assign_hook,
 						 GucShowHook show_hook);
