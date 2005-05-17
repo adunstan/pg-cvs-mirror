484203b680d922a7d38bab75c06c89fc2ec47dbd
Tom Lane <tgl@postgresql.org>
2005-05-17 18:26:23 +0000

    Add a --dbname option to the pg_regress script, and use pl_regression for testing PLs and contrib_regression for testing contrib, instead of overwriting the core system's regression database as formerly done. Andrew Dunstan

--- contrib/contrib-global.mk
+++ contrib/contrib-global.mk
@@ -1,4 +1,5 @@
-# $PostgreSQL: pgsql-server/contrib/contrib-global.mk,v 1.7 2003/11/29 19:51:18 pgsql Exp $
+# $PostgreSQL: pgsql/contrib/contrib-global.mk,v 1.8 2004/07/30 12:26:39 petere Exp $
 
 NO_PGXS = 1
+REGRESS_OPTS = --dbname=$(CONTRIB_TESTDB)
 include $(top_srcdir)/src/makefiles/pgxs.mk
