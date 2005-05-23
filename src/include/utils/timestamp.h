ee11c574fea3153446c6c17a342d0cf9fd641e69
Bruce Momjian <momjian@postgresql.org>
2005-05-23 18:56:55 +0000

    Add datetime macros for constants, for clarity:
    #define SECS_PER_DAY  86400
    #define USECS_PER_DAY INT64CONST(86400000000)
    #define USECS_PER_HOUR    INT64CONST(3600000000)
    #define USECS_PER_MINUTE INT64CONST(60000000)
    #define USECS_PER_SEC INT64CONST(1000000)

--- src/include/utils/timestamp.h
+++ src/include/utils/timestamp.h
@@ -6,7 +6,7 @@
  * Portions Copyright (c) 1996-2005, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/utils/timestamp.h,v 1.39 2004/08/29 05:06:59 momjian Exp $
+ * $PostgreSQL: pgsql/src/include/utils/timestamp.h,v 1.40 2004/12/31 22:03:46 pgsql Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -59,6 +59,11 @@ typedef struct
 #define MAX_TIMESTAMP_PRECISION 6
 #define MAX_INTERVAL_PRECISION 6
 
+#define SECS_PER_DAY	86400
+#define USECS_PER_DAY	INT64CONST(86400000000)
+#define USECS_PER_HOUR	INT64CONST(3600000000)
+#define USECS_PER_MINUTE INT64CONST(60000000)
+#define USECS_PER_SEC	INT64CONST(1000000)
 
 /*
  * Macros for fmgr-callable functions.
