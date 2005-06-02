0262096469ab643eef41572264f838bae670c1d6
Michael Meskes <meskes@postgresql.org>
2005-06-02 12:50:30 +0000

    Fixed memory leak in ecpglib by adding some missing free() commands.

--- src/interfaces/ecpg/ecpglib/execute.c
+++ src/interfaces/ecpg/ecpglib/execute.c
@@ -1,4 +1,4 @@
-/* $PostgreSQL: pgsql-server/src/interfaces/ecpg/ecpglib/execute.c,v 1.32 2004/01/28 09:52:14 meskes Exp $ */
+/* $PostgreSQL: pgsql-server/src/interfaces/ecpg/ecpglib/execute.c,v 1.26.2.4 2004/01/28 09:55:53 meskes Exp $ */
 
 /*
  * The aim is to get a simpler inteface to the database routines.
@@ -66,6 +66,7 @@ quote_postgres(char *arg, int lineno)
 	res[ri++] = '\'';
 	res[ri] = '\0';
 
+	ECPGfree(arg);
 	return res;
 }
 
@@ -799,8 +800,6 @@ ECPGstore_input(const struct statement * stmt, const struct variable * var,
 					if (!mallocedval)
 						return false;
 
-					ECPGfree(newcopy);
-
 					*tobeinserted_p = mallocedval;
 					*malloced_p = true;
 				}
@@ -834,8 +833,6 @@ ECPGstore_input(const struct statement * stmt, const struct variable * var,
 					mallocedval = quote_postgres(newcopy, stmt->lineno);
 					if (!mallocedval)
 						return false;
-
-					ECPGfree(newcopy);
 
 					*tobeinserted_p = mallocedval;
 					*malloced_p = true;
