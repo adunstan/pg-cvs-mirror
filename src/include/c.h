dec8d141ffcec1253db33e9b71fb066ed63892a3
Tom Lane <tgl@postgresql.org>
2005-06-08 15:50:28 +0000

    Change WAL-logging scheme for multixacts to be more like regular transaction IDs, rather than like subtrans; in particular, the information now survives a database restart.  Per previous discussion, this is essential for PITR log shipping and for 2PC.

--- src/include/c.h
+++ src/include/c.h
@@ -12,7 +12,7 @@
  * Portions Copyright (c) 1996-2005, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/c.h,v 1.183 2005/05/11 01:26:02 neilc Exp $
+ * $PostgreSQL: pgsql/src/include/c.h,v 1.184 2005/05/25 21:40:41 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -387,6 +387,8 @@ typedef uint32 SubTransactionId;
 
 /* MultiXactId must be equivalent to TransactionId, to fit in t_xmax */
 typedef TransactionId MultiXactId;
+
+typedef uint32 MultiXactOffset;
 
 typedef uint32 CommandId;
 
