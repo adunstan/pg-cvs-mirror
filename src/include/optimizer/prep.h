1c019d8092c0d8dc0f08bbca20d690f8d8950315
Tom Lane <tgl@postgresql.org>
2005-06-10 02:21:05 +0000

    If a LIMIT is applied to a UNION ALL query, plan each UNION arm as if the limit were directly applied to it.  This does not actually add a LIMIT plan node to the generated subqueries --- that would be useless overhead --- but it does cause the planner to prefer fast- start plans when the limit is small.  After an idea from Phil Endecott.

--- src/include/optimizer/prep.h
+++ src/include/optimizer/prep.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2005, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/optimizer/prep.h,v 1.49 2005/03/28 00:58:26 tgl Exp $
+ * $PostgreSQL: pgsql/src/include/optimizer/prep.h,v 1.50 2005/06/05 22:32:58 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -45,7 +45,8 @@ extern List *preprocess_targetlist(PlannerInfo *root, List *tlist);
 /*
  * prototypes for prepunion.c
  */
-extern Plan *plan_set_operations(PlannerInfo *root, List **sortClauses);
+extern Plan *plan_set_operations(PlannerInfo *root, double tuple_fraction,
+								 List **sortClauses);
 
 extern List *find_all_inheritors(Oid parentrel);
 
