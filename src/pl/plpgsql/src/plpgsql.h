0463f1f14e103f5f9dafee8fc8719081316f6b4e
Neil Conway <neilc@postgresql.org>
2005-06-14 06:43:15 +0000

    Allow the parameters to PL/PgSQL's RAISE statement to be expressions, instead of just scalar variables. Add regression tests and update the documentation. Along the way, remove some redundant error checking code from exec_stmt_perform().
    Original patch from Pavel Stehule, reworked by Neil Conway.

--- src/pl/plpgsql/src/plpgsql.h
+++ src/pl/plpgsql/src/plpgsql.h
@@ -3,7 +3,7 @@
  *			  procedural language
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/pl/plpgsql/src/plpgsql.h,v 1.61 2005/06/07 02:47:18 neilc Exp $
+ *	  $PostgreSQL: pgsql/src/pl/plpgsql/src/plpgsql.h,v 1.62 2005/06/10 16:23:11 neilc Exp $
  *
  *	  This software is copyrighted by Jan Wieck - Hamburg.
  *
@@ -515,7 +515,7 @@ typedef struct
 	int			lineno;
 	int			elog_level;
 	char	   *message;
-	List	   *params;
+	List	   *params;			/* list of expressions */
 } PLpgSQL_stmt_raise;
 
 
