e81eee60d053b7df256d8da349e81f40a7b90fd4
Tom Lane <tgl@postgresql.org>
2005-08-02 01:35:23 +0000

    Back-port several small portability fixes to get contrib building cleanly on AIX in the 8.0 branch.  Rocco Altier

--- src/port/getopt_long.c
+++ src/port/getopt_long.c
@@ -35,18 +35,21 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $PostgreSQL: /cvsroot/pgsql-server/src/port/getopt_long.c,v 1.2 2003/08/04 00:43:33 momjian Exp $
+ * $PostgreSQL: pgsql/src/port/getopt_long.c,v 1.3 2003/11/29 19:52:13 pgsql Exp $
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "c.h"
 
 #include "getopt_long.h"
+
+#ifndef HAVE_INT_OPTRESET
+int			optreset;
+#endif
 
 #define BADCH	'?'
 #define BADARG	':'
 #define EMSG	""
+
 
 int
 getopt_long(int argc, char *const argv[],
