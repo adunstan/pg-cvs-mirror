04614ac8b3eda166886bb9b1c0eaff7636faf98a
Bruce Momjian <momjian@postgresql.org>
2005-08-28 18:49:01 +0000

    Use GetCurrentThreadId, per Magnus.

--- src/interfaces/libpq/pthread-win32.c
+++ src/interfaces/libpq/pthread-win32.c
@@ -5,7 +5,7 @@
 *
 * Copyright (c) 2004-2005, PostgreSQL Global Development Group
 * IDENTIFICATION
-*	$PostgreSQL: pgsql/src/interfaces/libpq/pthread-win32.c,v 1.7 2005/08/23 20:48:47 momjian Exp $
+*	$PostgreSQL: pgsql/src/interfaces/libpq/pthread-win32.c,v 1.8 2005/08/23 21:02:03 momjian Exp $
 *
 *-------------------------------------------------------------------------
 */
@@ -17,7 +17,7 @@
 HANDLE
 pthread_self()
 {
-	return GetCurrentThread();
+	return GetCurrentThreadId();
 }
 
 void
