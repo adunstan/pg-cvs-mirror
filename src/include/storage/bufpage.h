f349b084ffdcc98d68f7310fa0666c3f2193d9a3
Bruce Momjian <momjian@postgresql.org>
2005-09-22 16:46:00 +0000

    This patch cleans up the access to members of ItemIdData. It uses existing macros instead of touching directly.
    ITAGAKI Takahiro

--- src/include/storage/bufpage.h
+++ src/include/storage/bufpage.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2005, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/storage/bufpage.h,v 1.64 2005/03/22 06:17:03 tgl Exp $
+ * $PostgreSQL: pgsql/src/include/storage/bufpage.h,v 1.65 2005/04/28 21:47:18 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -258,8 +258,8 @@ typedef PageHeaderData *PageHeader;
 #define PageGetItem(page, itemId) \
 ( \
 	AssertMacro(PageIsValid(page)), \
-	AssertMacro((itemId)->lp_flags & LP_USED), \
-	(Item)(((char *)(page)) + (itemId)->lp_off) \
+	AssertMacro(ItemIdIsUsed(itemId)), \
+	(Item)(((char *)(page)) + ItemIdGetOffset(itemId)) \
 )
 
 /*
--- src/include/storage/itemid.h
+++ src/include/storage/itemid.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2005, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/storage/itemid.h,v 1.23 2004/08/29 04:13:10 momjian Exp $
+ * $PostgreSQL: pgsql/src/include/storage/itemid.h,v 1.24 2004/12/31 22:03:42 pgsql Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -38,7 +38,7 @@ typedef ItemIdData *ItemId;
 
 /*
  * This bit may be passed to PageAddItem together with
- * LP_USED & LP_DELETED bits to specify overwrite mode
+ * LP_USED & LP_DELETE bits to specify overwrite mode
  */
 #define OverwritePageMode	0x10
 
