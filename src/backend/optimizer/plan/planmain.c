9ad4ef3005b7dd6603c9b3c615d2f6b90a78b3b6
Tom Lane <tgl@postgresql.org>
2005-09-28 21:17:02 +0000

    Repair planning bug introduced in 7.4: outer-join ON clauses that referenced only the inner-side relation would be considered as potential equijoin clauses, which is wrong because the condition doesn't necessarily hold above the point of the outer join.  Per test case from Kevin Grittner (bug#1916).

--- src/backend/optimizer/plan/planmain.c
+++ src/backend/optimizer/plan/planmain.c
@@ -14,7 +14,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/optimizer/plan/planmain.c,v 1.86 2005/07/02 23:00:41 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/backend/optimizer/plan/planmain.c,v 1.87 2005/08/27 22:13:43 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -155,7 +155,7 @@ query_planner(PlannerInfo *root, List *tlist, double tuple_fraction,
 	 */
 	build_base_rel_tlists(root, tlist);
 
-	(void) distribute_quals_to_rels(root, (Node *) parse->jointree);
+	(void) distribute_quals_to_rels(root, (Node *) parse->jointree, false);
 
 	/*
 	 * Use the completed lists of equijoined keys to deduce any implied
