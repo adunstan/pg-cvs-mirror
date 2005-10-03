e73b8950d39e2dfc99808185a56e9420f1ff93b4
Tom Lane <tgl@postgresql.org>
2005-10-03 23:43:29 +0000

    COPY's test for read-only transaction was backward; it prohibited COPY TO where it should prohibit COPY FROM.  Found by Alon Goldshuv.

--- src/backend/commands/copy.c
+++ src/backend/commands/copy.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/commands/copy.c,v 1.236 2004/12/31 21:59:41 pgsql Exp $
+ *	  $PostgreSQL: pgsql/src/backend/commands/copy.c,v 1.236.4.1 2005/05/13 06:35:25 neilc Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -900,7 +900,8 @@ DoCopy(const CopyStmt *stmt)
 	rel = heap_openrv(relation, (is_from ? RowExclusiveLock : AccessShareLock));
 
 	/* check read-only transaction */
-	if (XactReadOnly && !is_from && !isTempNamespace(RelationGetNamespace(rel)))
+	if (XactReadOnly && is_from &&
+		!isTempNamespace(RelationGetNamespace(rel)))
 		ereport(ERROR,
 				(errcode(ERRCODE_READ_ONLY_SQL_TRANSACTION),
 				 errmsg("transaction is read-only")));
