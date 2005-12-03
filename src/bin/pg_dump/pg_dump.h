a2a13b233d5d2843a1571d56c7001b7f74c680a9
Tom Lane <tgl@postgresql.org>
2005-12-03 21:06:18 +0000

    Treat procedural languages as owned by the bootstrap superuser, rather than owned by nobody.  This results in cleaner display of language ACLs, since the backend's aclchk.c uses the same convention.  AFAICS there is no practical difference but it's nice to avoid emitting SET SESSION AUTHORIZATION; also this will make it easier to transition pg_dump to some future version in which we may include an explicit ownership column in pg_language.  Per gripe from David Begley.

--- src/bin/pg_dump/pg_dump.h
+++ src/bin/pg_dump/pg_dump.h
@@ -6,7 +6,7 @@
  * Portions Copyright (c) 1996-2005, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/bin/pg_dump/pg_dump.h,v 1.121 2005/09/05 23:50:49 tgl Exp $
+ * $PostgreSQL: pgsql/src/bin/pg_dump/pg_dump.h,v 1.122 2005/10/15 02:49:39 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -297,6 +297,7 @@ typedef struct _procLangInfo
 	Oid			lanplcallfoid;
 	Oid			lanvalidator;
 	char	   *lanacl;
+	char	   *lanowner;		/* name of owner, or empty string */
 } ProcLangInfo;
 
 typedef struct _castInfo
