cae2023b3cc735d77575ea7b4c79c62acfd1828a
Tom Lane <tgl@postgresql.org>
2005-12-10 01:09:14 +0000

    Remove incorrect increment of lineno, per David Fetter. Sync HEAD and 8.1 branches of pgbench.

--- contrib/pgbench/pgbench.c
+++ contrib/pgbench/pgbench.c
@@ -1,5 +1,5 @@
 /*
- * $PostgreSQL: pgsql/contrib/pgbench/pgbench.c,v 1.45.2.2 2005/11/23 13:23:34 ishii Exp $
+ * $PostgreSQL: pgsql/contrib/pgbench/pgbench.c,v 1.48 2005/11/23 12:19:12 ishii Exp $
  *
  * pgbench: a simple benchmark program for PostgreSQL
  * written by Tatsuo Ishii
@@ -898,10 +898,8 @@ process_file(char *filename)
 				fclose(fd);
 				return false;
 			}
-		} else {
-			lineno++;
+		} else
 			continue;
-		}
 
 		my_commands[lineno] = commands;
 		lineno++;
@@ -1120,8 +1118,7 @@ main(int argc, char **argv)
 					fprintf(stderr, "Use limit/ulimt to increase the limit before using pgbench.\n");
 					exit(1);
 				}
-#endif   /* #if !(defined(__CYGWIN__) ||
-								 * defined(__MINGW32__)) */
+#endif
 				break;
 			case 'C':
 				is_connect = 1;
