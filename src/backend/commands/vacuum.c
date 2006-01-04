d79560839f2205f2631bc6f607dc4ae8c6095124
Tom Lane <tgl@postgresql.org>
2006-01-04 19:16:24 +0000

    Fix another case in which autovacuum would fail while analyzing expressional indexes.  Per report from Brian Hirt.

--- src/backend/commands/vacuum.c
+++ src/backend/commands/vacuum.c
@@ -13,7 +13,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/commands/vacuum.c,v 1.318 2005/11/20 19:49:07 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/backend/commands/vacuum.c,v 1.319 2005/11/22 18:17:09 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -510,6 +510,14 @@ vacuum(VacuumStmt *vacstmt, List *relids)
 		 * PostgresMain().
 		 */
 		StartTransactionCommand();
+		/*
+		 * Re-establish the transaction snapshot.  This is wasted effort
+		 * when we are called as a normal utility command, because the
+		 * new transaction will be dropped immediately by PostgresMain();
+		 * but it's necessary if we are called from autovacuum because
+		 * autovacuum might continue on to do an ANALYZE-only call.
+		 */
+		ActiveSnapshot = CopySnapshot(GetTransactionSnapshot());
 	}
 
 	if (vacstmt->vacuum)
