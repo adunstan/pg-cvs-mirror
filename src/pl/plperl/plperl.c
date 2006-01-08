4149a3d3ef436f2ca746747a0129afe61ef518de
Andrew Dunstan <adunstan@postgresql.org>
2006-01-08 15:51:19 +0000

    Stop perl from hijacking stdio and other stuff on Windows.

--- src/pl/plperl/plperl.c
+++ src/pl/plperl/plperl.c
@@ -33,7 +33,7 @@
  *	  ENHANCEMENTS, OR MODIFICATIONS.
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/pl/plperl/plperl.c,v 1.67.4.2 2005/07/03 21:56:27 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/pl/plperl/plperl.c,v 1.67.4.3 2005/08/20 19:19:31 tgl Exp $
  *
  **********************************************************************/
 
@@ -52,6 +52,12 @@
 #include "utils/typcache.h"
 
 /* perl stuff */
+
+/* stop perl from hijacking stdio and other stuff */
+#ifdef WIN32
+#define WIN32IO_IS_STDIO
+#endif 
+
 #include "EXTERN.h"
 #include "perl.h"
 #include "XSUB.h"
