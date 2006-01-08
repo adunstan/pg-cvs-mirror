bf7b11a811fccbd6fd6611e8b7b944d0b49dde4e
Andrew Dunstan <adunstan@postgresql.org>
2006-01-08 22:27:52 +0000

    Rationalise perl header inclusions via a common include file, which also declares routines in plperl.c and spi_internal.c used in other files.
    Along the way, also stop perl from hijacking stdio and other stuff on Windows.

--- src/pl/plperl/plperl.c
+++ src/pl/plperl/plperl.c
@@ -33,7 +33,7 @@
  *	  ENHANCEMENTS, OR MODIFICATIONS.
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/pl/plperl/plperl.c,v 1.97 2005/12/28 18:34:16 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/pl/plperl/plperl.c,v 1.98 2005/12/29 14:28:31 adunstan Exp $
  *
  **********************************************************************/
 
@@ -60,18 +60,7 @@
 extern DLLIMPORT bool check_function_bodies;
 
 /* perl stuff */
-#include "EXTERN.h"
-#include "perl.h"
-#include "XSUB.h"
-#include "ppport.h"
-#include "spi_internal.h"
-
-/* just in case these symbols aren't provided */
-#ifndef pTHX_
-#define pTHX_
-#define pTHX void
-#endif
-
+#include "plperl.h"
 
 /**********************************************************************
  * The information we cache about loaded procedures
