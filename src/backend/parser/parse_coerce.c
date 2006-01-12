522b852bb6c8a76d521ab8c43a77716a7f502d21
Neil Conway <neilc@postgresql.org>
2006-01-12 22:29:11 +0000

    We neglected to apply domain constraints on UNKNOWN parameters to prepared statements, per report from David Wheeler.

--- src/backend/parser/parse_coerce.c
+++ src/backend/parser/parse_coerce.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/parser/parse_coerce.c,v 2.132 2005/10/15 02:49:22 momjian Exp $
+ *	  $PostgreSQL: pgsql/src/backend/parser/parse_coerce.c,v 2.132.2.1 2005/11/22 18:23:13 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -243,7 +243,10 @@ coerce_type(ParseState *pstate, Node *node,
 		}
 
 		param->paramtype = targetTypeId;
-		return (Node *) param;
+
+		/* Apply domain constraints, if necessary */
+		return coerce_to_domain((Node *) param, InvalidOid, targetTypeId,
+								cformat, false, false);
 	}
 	if (find_coercion_pathway(targetTypeId, inputTypeId, ccontext,
 							  &funcId))
