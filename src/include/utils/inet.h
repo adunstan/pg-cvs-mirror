851ef0b05ec69152a31f7cd3677412399922db18
Bruce Momjian <momjian@postgresql.org>
2006-01-23 21:45:47 +0000

    Use is_cidr in INET/CIDR structure, rather than the generic 'type'.

--- src/include/utils/inet.h
+++ src/include/utils/inet.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2005, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/utils/inet.h,v 1.19 2004/08/29 04:13:11 momjian Exp $
+ * $PostgreSQL: pgsql/src/include/utils/inet.h,v 1.20 2004/12/31 22:03:46 pgsql Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -22,7 +22,7 @@ typedef struct
 {
 	unsigned char family;		/* PGSQL_AF_INET or PGSQL_AF_INET6 */
 	unsigned char bits;			/* number of bits in netmask */
-	unsigned char type;			/* 0 = inet, 1 = cidr */
+	bool is_cidr;				/* is cidr? */
 	unsigned char ipaddr[16];	/* up to 128 bits of address */
 } inet_struct;
 
