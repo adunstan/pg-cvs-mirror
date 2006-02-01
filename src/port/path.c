adfdce68017a76ea82a9aaf5f2b6a56de997126f
Bruce Momjian <momjian@postgresql.org>
2006-02-01 12:41:45 +0000

    Fix const cast in get_progname().
    Backpatch.

--- src/port/path.c
+++ src/port/path.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/port/path.c,v 1.63 2005/12/23 22:34:22 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/port/path.c,v 1.64 2006/02/01 00:31:59 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -389,7 +389,7 @@ const char *
 get_progname(const char *argv0)
 {
 	const char  *nodir_name;
-	const char	*progname;
+	char		*progname;
 
 	nodir_name = last_dir_separator(argv0);
 	if (nodir_name)
