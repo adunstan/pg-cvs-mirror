b858622ac3094c2ea1ca0ff18c7bcfcd8d251530
Bruce Momjian <momjian@postgresql.org>
2006-02-11 21:55:35 +0000

    o Improve psql's handling of multi-line statements
    	Currently, while \e saves a single statement as one entry, interactive
    	statements are saved one line at a time.  Ideally all statements
    	would be saved like \e does.
    
    Sergey E. Koposov

--- src/bin/psql/prompt.c
+++ src/bin/psql/prompt.c
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 2000-2005, PostgreSQL Global Development Group
  *
- * $PostgreSQL: pgsql/src/bin/psql/prompt.c,v 1.40 2005/10/15 02:49:40 momjian Exp $
+ * $PostgreSQL: pgsql/src/bin/psql/prompt.c,v 1.41 2006/01/03 23:32:30 tgl Exp $
  */
 #include "postgres_fe.h"
 #include "prompt.h"
@@ -12,6 +12,7 @@
 
 #include "settings.h"
 #include "common.h"
+#include "pqexpbuffer.h"
 #include "input.h"
 #include "variables.h"
 
