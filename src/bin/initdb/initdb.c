b5b6cb17a02eb2b2a85660c27146199c11df1958
Peter Eisentraut <petere@postgresql.org>
2006-02-18 16:15:23 +0000

    Add support for Windows codepages 1253, 1254, 1255, and 1257 and clean up a bunch of the support utilities.
    In src/backend/utils/mb/Unicode remove nearly duplicate copies of the
    UCS_to_XXX perl script and replace with one version to handle all generic
    files.  Update the Makefile so that it knows about all the map files.
    This produces a slight difference in some of the map files, using a
    uniform naming convention and not mapping the null character.
    
    In src/backend/utils/mb/conversion_procs create a master utf8<->win
    codepage function like the ISO 8859 versions instead of having a separate
    handler for each conversion.
    
    There is an externally visible change in the name of the win1258 to utf8
    conversion.  According to the documentation notes, it was named
    incorrectly and this changes it to a standard name.
    
    Running the Unicode mapping perl scripts has shown some additional mapping
    changes in koi8r and iso8859-7.

--- src/bin/initdb/initdb.c
+++ src/bin/initdb/initdb.c
@@ -42,7 +42,7 @@
  * Portions Copyright (c) 1994, Regents of the University of California
  * Portions taken from FreeBSD.
  *
- * $PostgreSQL: pgsql/src/bin/initdb/initdb.c,v 1.108 2006/02/10 22:05:42 tgl Exp $
+ * $PostgreSQL: pgsql/src/bin/initdb/initdb.c,v 1.109 2006/02/12 03:22:18 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -779,7 +779,11 @@ struct encoding_match encoding_match_list[] = {
 	{PG_LATIN10, "iso885916"},
 
 	{PG_WIN1252, "CP1252"},
+	{PG_WIN1253, "CP1253"},
+	{PG_WIN1254, "CP1254"},
+	{PG_WIN1255, "CP1255"},
 	{PG_WIN1256, "CP1256"},
+	{PG_WIN1257, "CP1257"},
 	{PG_WIN1258, "CP1258"},
 #ifdef NOT_VERIFIED
 	{PG_WIN874, "???"},
