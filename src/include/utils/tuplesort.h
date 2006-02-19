df18d84ba2f60b45d6e2fcecc85dd8f742803b2b
Tom Lane <tgl@postgresql.org>
2006-02-19 05:54:06 +0000

    Improve tuplesort.c to support variable merge order.  The original coding with fixed merge order (fixed number of "tapes") was based on obsolete assumptions, namely that tape drives are expensive.  Since our "tapes" are really just a couple of buffers, we can have a lot of them given adequate workspace.  This allows reduction of the number of merge passes with consequent savings of I/O during large sorts.
    Simon Riggs with some rework by Tom Lane

--- src/include/utils/tuplesort.h
+++ src/include/utils/tuplesort.h
@@ -13,7 +13,7 @@
  * Portions Copyright (c) 1996-2005, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/utils/tuplesort.h,v 1.16 2004/08/29 04:13:11 momjian Exp $
+ * $PostgreSQL: pgsql/src/include/utils/tuplesort.h,v 1.17 2004/12/31 22:03:46 pgsql Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -66,6 +66,8 @@ extern bool tuplesort_getdatum(Tuplesortstate *state, bool forward,
 				   Datum *val, bool *isNull);
 
 extern void tuplesort_end(Tuplesortstate *state);
+
+extern int	tuplesort_merge_order(long allowedMem);
 
 /*
  * These routines may only be called if randomAccess was specified 'true'.
