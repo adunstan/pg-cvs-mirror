8afbecceca6c716aece1e8422c6fc880f89aaa9e
Bruce Momjian <momjian@postgresql.org>
2006-03-03 21:52:37 +0000

    Update ipcclean to use try 'id' first for root check.

--- src/bin/ipcclean/ipcclean.sh
+++ src/bin/ipcclean/ipcclean.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $PostgreSQL: pgsql/src/bin/ipcclean/ipcclean.sh,v 1.16 2006/01/05 01:56:29 momjian Exp $
+# $PostgreSQL: pgsql/src/bin/ipcclean/ipcclean.sh,v 1.17 2006/03/03 16:49:21 momjian Exp $
 #
 
 CMDNAME=`basename $0`
@@ -19,8 +19,19 @@ if [ "$1" = '-?' -o "$1" = "--help" ]; then
     exit 0
 fi
 
-# only check $LOGNAME if $USER is not set
-if [ "$USER" = 'root' -o \( ! "$USER" -a "$LOGNAME" = 'root' \) ]
+# test for running as root
+
+ISROOT="N"
+if id -u >/dev/null 2>&1
+then	if [ `id -u` -eq 0 ]
+	then	ISROOT="Y"
+	fi
+elif	# only check $LOGNAME if $USER is not set
+	[ "$USER" = 'root' -o \( ! "$USER" -a "$LOGNAME" = 'root' \) ]
+then	ISROOT="Y"
+fi
+
+if [ "$ISROOT" = "Y" ]
 then
   (
     echo "$CMDNAME: cannot be run as root" 1>&2
