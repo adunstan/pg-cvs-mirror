33593a482d3cd3c852c787f463ae9480b8134ee9
Bruce Momjian <momjian@postgresql.org>
2006-03-05 05:33:19 +0000

    Check for "msys" so it doesn't use 'con' by checking for an evironment variable.

--- src/port/sprompt.c
+++ src/port/sprompt.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/port/sprompt.c,v 1.12.2.1 2006/03/03 23:49:21 momjian Exp $
+ *	  $PostgreSQL: pgsql/src/port/sprompt.c,v 1.12.2.2 2006/03/04 04:30:51 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -65,7 +65,12 @@ simple_prompt(const char *prompt, int maxlen, bool echo)
 	 */
 	termin = fopen(DEVTTY, "r");
 	termout = fopen(DEVTTY, "w");
-	if (!termin || !termout)
+	if (!termin || !termout
+#ifdef WIN32
+		/* See DEVTTY comment for msys */
+		|| (getenv("OSTYPE") && strcmp(getenv("OSTYPE"), "msys") == 0)
+#endif
+		)
 	{
 		if (termin)
 			fclose(termin);
