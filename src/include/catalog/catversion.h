df45ca625de393b86af16b1aba12fe1be4724beb
Bruce Momjian <momjian@postgresql.org>
2006-03-06 22:49:17 +0000

    Attached is the new patch.  To summarize:
       - new function justify_interval(interval)
       - modified function justify_hours(interval)
       - modified function justify_days(interval)
    
    These functions are defined to meet the requirements as discussed in
    this thread.  Specifically:
    
       - justify_hours makes certain the sign bit on the hours
         matches the sign bit on the days.  It only checks the
         sign bit on the days, and not the months, when
         determining if the hours should be positive or negative.
         After the call, -24 < hours < 24.
    
       - justify_days makes certain the sign bit on the days
         matches the sign bit on the months.  It's behavior does
         not depend on the hours, nor does it modify the hours.
         After the call, -30 < days < 30.
    
       - justify_interval makes sure the sign bits on all three
         fields months, days, and hours are all the same.  After
         the call, -24 < hours < 24 AND -30 < days < 30.
    
    Mark Dilger

--- src/include/catalog/catversion.h
+++ src/include/catalog/catversion.h
@@ -37,7 +37,7 @@
  * Portions Copyright (c) 1996-2006, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/catalog/catversion.h,v 1.317 2006/02/28 22:37:26 tgl Exp $
+ * $PostgreSQL: pgsql/src/include/catalog/catversion.h,v 1.318 2006/03/05 15:58:54 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -53,6 +53,6 @@
  */
 
 /*							yyyymmddN */
-#define CATALOG_VERSION_NO	200602281
+#define CATALOG_VERSION_NO	200603061
 
 #endif
