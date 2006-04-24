a2b85699de945cd6f63524d3d92fcab5f3987a49
Bruce Momjian <momjian@postgresql.org>
2006-04-24 22:06:32 +0000

    Suprress compiler warning in gcc 4.2.
    Report by Kris Jurka

--- src/include/access/heapam.h
+++ src/include/access/heapam.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2006, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/access/heapam.h,v 1.106 2006/03/05 15:58:53 momjian Exp $
+ * $PostgreSQL: pgsql/src/include/access/heapam.h,v 1.107 2006/03/24 04:32:13 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -100,7 +100,7 @@ extern Datum fastgetattr(HeapTuple tup, int attnum, TupleDesc tupleDesc,
 		( \
 			((attnum) > (int) (tup)->t_data->t_natts) ? \
 			( \
-				((isnull) ? (*(isnull) = true) : (dummyret)NULL), \
+				((isnull) != NULL) ? (*(isnull) = true) : (dummyret)NULL), \
 				(Datum)NULL \
 			) \
 			: \
