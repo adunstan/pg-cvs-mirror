dd8d0586aecefebd7e3503e69498860f9b946821
Tom Lane <tgl@postgresql.org>
2006-05-11 21:58:37 +0000

    Remove unnecessary .seg/.section directives, per Alan Stange.

--- src/backend/storage/lmgr/s_lock.c
+++ src/backend/storage/lmgr/s_lock.c
@@ -9,7 +9,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/storage/lmgr/s_lock.c,v 1.35 2004/12/31 22:01:05 pgsql Exp $
+ *	  $PostgreSQL: pgsql/src/backend/storage/lmgr/s_lock.c,v 1.35.4.1 2005/08/26 14:48:13 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -217,15 +217,6 @@ static void
 tas_dummy()						/* really means: extern int tas(slock_t
 								 * *lock); */
 {
-
-#ifdef SUNOS4_CC
-	asm(".seg \"data\"");
-	asm(".seg \"text\"");
-#else
-	asm(".section \"data\"");
-	asm(".section \"text\"");
-#endif
-
 	asm("_tas:");
 
 	/*
