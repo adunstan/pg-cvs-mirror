13897d758efe42764a3bd7cefb0e5b4e7c046906
Bruce Momjian <momjian@postgresql.org>
2006-05-17 01:44:24 +0000

    Rename macro parameter, for clarity.

--- src/include/libpq/pqcomm.h
+++ src/include/libpq/pqcomm.h
@@ -9,7 +9,7 @@
  * Portions Copyright (c) 1996-2006, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/libpq/pqcomm.h,v 1.98 2005/10/15 02:49:44 momjian Exp $
+ * $PostgreSQL: pgsql/src/include/libpq/pqcomm.h,v 1.99 2006/03/05 15:58:56 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -76,9 +76,9 @@ typedef struct
 
 /* Configure the UNIX socket location for the well known port. */
 
-#define UNIXSOCK_PATH(path,port,defpath) \
+#define UNIXSOCK_PATH(path, port, sockdir) \
 		snprintf(path, sizeof(path), "%s/.s.PGSQL.%d", \
-				((defpath) && *(defpath) != '\0') ? (defpath) : \
+				((sockdir) && *(sockdir) != '\0') ? (sockdir) : \
 				DEFAULT_PGSOCKET_DIR, \
 				(port))
 
