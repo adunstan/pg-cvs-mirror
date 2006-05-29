82c2991385e8a891ff3af956cdece30f0c83dfa8
Andrew Dunstan <adunstan@postgresql.org>
2006-05-29 13:51:23 +0000

    Make plperl's $_TD trigger data a global rather than a lexical variable, with a fresh local value for each invocation, to avoid unexpected sharing violations. Per recent -hackers discussion.

--- src/pl/plperl/plperl.c
+++ src/pl/plperl/plperl.c
@@ -1,7 +1,7 @@
 /**********************************************************************
  * plperl.c - perl as a procedural language for PostgreSQL
  *
- *	  $PostgreSQL: pgsql/src/pl/plperl/plperl.c,v 1.108 2006/04/04 19:35:37 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/pl/plperl/plperl.c,v 1.109 2006/05/26 17:34:16 adunstan Exp $
  *
  **********************************************************************/
 
@@ -771,7 +771,7 @@ plperl_create_sub(char *s, bool trusted)
 	ENTER;
 	SAVETMPS;
 	PUSHMARK(SP);
-	XPUSHs(sv_2mortal(newSVpv("my $_TD=$_[0]; shift;", 0)));
+	XPUSHs(sv_2mortal(newSVpv("our $_TD; local $_TD=$_[0]; shift;", 0)));
 	XPUSHs(sv_2mortal(newSVpv(s, 0)));
 	PUTBACK;
 
