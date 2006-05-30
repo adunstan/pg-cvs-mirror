4b01fdcb583a66a0d7c9a368f4df34f12e7c8bdd
Tom Lane <tgl@postgresql.org>
2006-05-30 13:58:49 +0000

    Clarify the HINT for 'checkpoint request failed', per recent complaint demonstrating that its intent wasn't obvious.

--- src/backend/postmaster/bgwriter.c
+++ src/backend/postmaster/bgwriter.c
@@ -37,7 +37,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/postmaster/bgwriter.c,v 1.22 2005/12/08 19:19:22 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/backend/postmaster/bgwriter.c,v 1.23 2006/03/05 15:58:35 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -613,7 +613,7 @@ RequestCheckpoint(bool waitforit, bool warnontime)
 		if (bgs->ckpt_failed != old_failed)
 			ereport(ERROR,
 					(errmsg("checkpoint request failed"),
-					 errhint("Consult the server log for details.")));
+					 errhint("Consult recent messages in the server log for details.")));
 	}
 }
 
