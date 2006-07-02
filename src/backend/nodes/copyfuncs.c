8a6109f23875806e6187af7d83a7247964eb1f43
Bruce Momjian <momjian@postgresql.org>
2006-07-02 01:58:36 +0000

    ALTER TABLE ... ADD/DROPS INHERIT (actually INHERIT / NO INHERIT)
    Open items:
    
    There were a few tangentially related issues that have come up that I think
    are TODOs. I'm likely to tackle one or two of these next so I'm interested in
    hearing feedback on them as well.
    
    . Constraints currently do not know anything about inheritance. Tom suggested
      adding a coninhcount and conislocal like attributes have to track their
      inheritance status.
    
    . Foreign key constraints currently do not get copied to new children (and
      therefore my code doesn't verify them). I don't think it would be hard to
      add them and treat them like CHECK constraints.
    
    . No constraints at all are copied to tables defined with LIKE. That makes it
      hard to use LIKE to define new partitions. The standard defines LIKE and
      specifically says it does not copy constraints. But the standard already has
      an option called INCLUDING DEFAULTS; we could always define a non-standard
      extension LIKE table INCLUDING CONSTRAINTS that gives the user the option to
      request a copy including constraints.
    
    . Personally, I think the whole attislocal thing is bunk. The decision about
      whether to drop a column from children tables or not is something that
      should be up to the user and trying to DWIM based on whether there was ever
      a local definition or the column was acquired purely through inheritance is
      hardly ever going to match up with user expectations.
    
    . And of course there's the whole unique and primary key constraint issue. I
      think to get any traction at all on this you have a prerequisite of a real
      partitioned table implementation where the system knows what the partition
      key is so it can recognize when it's a leading part of an index key.
    
    Greg Stark

--- src/backend/nodes/copyfuncs.c
+++ src/backend/nodes/copyfuncs.c
@@ -15,7 +15,7 @@
  * Portions Copyright (c) 1994, Regents of the University of California
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/nodes/copyfuncs.c,v 1.337 2006/06/27 03:43:19 momjian Exp $
+ *	  $PostgreSQL: pgsql/src/backend/nodes/copyfuncs.c,v 1.338 2006/07/01 18:38:32 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -1800,6 +1800,7 @@ _copyAlterTableCmd(AlterTableCmd *from)
 	COPY_SCALAR_FIELD(subtype);
 	COPY_STRING_FIELD(name);
 	COPY_NODE_FIELD(def);
+	COPY_NODE_FIELD(parent);
 	COPY_NODE_FIELD(transform);
 	COPY_SCALAR_FIELD(behavior);
 
