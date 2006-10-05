eafce4b083440418e05112e5c8108aa90c4983b4
Tom Lane <tgl@postgresql.org>
2006-10-05 01:40:45 +0000

    Work around buggy strtod on (some versions of?) IRIX.  Combination of proposed patches from John Jorgensen and Steve Singer.

--- src/include/port/irix.h
+++ src/include/port/irix.h
@@ -1,1 +1,7 @@
-/* $PostgreSQL: pgsql/src/backend/utils/misc/guc.c,v 1.314 2006/03/07 02:54:23 momjian Exp $ */
+/* $PostgreSQL: pgsql/src/include/port/irix.h,v 1.3 2006/03/11 04:38:38 momjian Exp $ */
+
+/*
+ * IRIX 6.5.26f and 6.5.22f (at least) have a strtod() that accepts
+ * "infinity", but leaves endptr pointing to "inity".
+ */
+#define HAVE_BUGGY_IRIX_STRTOD
