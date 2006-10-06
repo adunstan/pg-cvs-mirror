623b719411b5de3c44fbaf9ddd5f05958fa34ea6
Tom Lane <tgl@postgresql.org>
2006-10-06 18:23:48 +0000

    Fix SysCacheGetAttr() to handle the case where the specified syscache has not been initialized yet.  This can happen because there are code paths that call SysCacheGetAttr() on a tuple originally fetched from a different syscache (hopefully on the same catalog) than the one specified in the call.  It doesn't seem useful or robust to try to prevent that from happening, so just improve the function to cope instead.  Per bug#2678 from Jeff Trout.  The specific example shown by Jeff is new in 8.1, but to be on the safe side I'm backpatching 8.0 as well.  We could patch 7.x similarly but I think that's probably overkill, given the lack of evidence of old bugs of this ilk.

--- src/include/utils/catcache.h
+++ src/include/utils/catcache.h
@@ -13,7 +13,7 @@
  * Portions Copyright (c) 1996-2005, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/utils/catcache.h,v 1.51 2004/08/29 04:13:10 momjian Exp $
+ * $PostgreSQL: pgsql/src/include/utils/catcache.h,v 1.52 2004/12/31 22:03:45 pgsql Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -167,7 +167,7 @@ extern void AtEOXact_CatCache(bool isCommit);
 extern CatCache *InitCatCache(int id, const char *relname, const char *indname,
 			 int reloidattr,
 			 int nkeys, const int *key);
-extern void InitCatCachePhase2(CatCache *cache);
+extern void InitCatCachePhase2(CatCache *cache, bool touch_index);
 
 extern HeapTuple SearchCatCache(CatCache *cache,
 			   Datum v1, Datum v2,
