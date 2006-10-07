d43f33c9747246eb6535ea613784d984f6d9d278
Tom Lane <tgl@postgresql.org>
2006-10-07 19:25:29 +0000

    On platforms that have getrlimit(RLIMIT_STACK), use it to ensure that max_stack_depth is not set to an unsafe value.
    This commit also provides configure-time checking for <sys/resource.h>,
    and cleans up some perhaps-unportable code associated with use of that
    include file and getrlimit().

--- src/include/utils/pg_rusage.h
+++ src/include/utils/pg_rusage.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2006, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/utils/pg_rusage.h,v 1.1 2005/10/03 22:52:26 tgl Exp $
+ * $PostgreSQL: pgsql/src/include/utils/pg_rusage.h,v 1.2 2006/03/05 15:59:07 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -16,7 +16,7 @@
 
 #include <sys/time.h>
 
-#ifdef HAVE_GETRUSAGE
+#ifdef HAVE_SYS_RESOURCE_H
 #include <sys/resource.h>
 #else
 #include "rusagestub.h"
