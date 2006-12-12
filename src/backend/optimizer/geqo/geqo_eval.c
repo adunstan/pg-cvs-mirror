cec17e558ad2f6ca605bd5b8fe67426c98451a63
Tom Lane <tgl@postgresql.org>
2006-12-12 21:31:09 +0000

    Fix planner to do the right thing when a degenerate outer join (one whose joinclause doesn't use any outer-side vars) requires a "bushy" plan to be created.  The normal heuristic to avoid joins with no joinclause has to be overridden in that case.  Problem is new in 8.2; before that we forced the outer join order anyway.  Per example from Teodor.

--- src/backend/optimizer/geqo/geqo_eval.c
+++ src/backend/optimizer/geqo/geqo_eval.c
@@ -6,7 +6,7 @@
  * Portions Copyright (c) 1996-2006, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/backend/optimizer/geqo/geqo_eval.c,v 1.80 2006/03/05 15:58:28 momjian Exp $
+ * $PostgreSQL: pgsql/src/backend/optimizer/geqo/geqo_eval.c,v 1.81 2006/10/24 17:50:22 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -258,7 +258,7 @@ desirable_join(PlannerInfo *root,
 	/*
 	 * Join if there is an applicable join clause.
 	 */
-	if (have_relevant_joinclause(outer_rel, inner_rel))
+	if (have_relevant_joinclause(root, outer_rel, inner_rel))
 		return true;
 
 	/*
