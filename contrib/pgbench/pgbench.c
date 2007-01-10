615c97923b5a1efd782f153473730447e2c52b8f
Tatsuo Ishii <ishii@postgresql.org>
2007-01-10 01:44:30 +0000

    Back port patch.
    Call srandom() instead of srand().
    pgbench calls random() later, so it should have called srandom().
    On most platforms except Windows srandom() is actually identical
    to srand(), so the bug only bites Windows users.
    per bug report from Akio Ishida.

--- contrib/pgbench/pgbench.c
+++ contrib/pgbench/pgbench.c
@@ -1,10 +1,10 @@
 /*
- * $PostgreSQL: pgsql/contrib/pgbench/pgbench.c,v 1.48 2005/11/23 12:19:12 ishii Exp $
+ * $PostgreSQL: pgsql/contrib/pgbench/pgbench.c,v 1.45.2.4 2005/12/10 01:09:14 tgl Exp $
  *
  * pgbench: a simple benchmark program for PostgreSQL
  * written by Tatsuo Ishii
  *
- * Copyright (c) 2000-2005	Tatsuo Ishii
+ * Copyright (c) 2000-2007	Tatsuo Ishii
  *
  * Permission to use, copy, modify, and distribute this software and
  * its documentation for any purpose and without fee is hereby
@@ -1296,7 +1296,7 @@ main(int argc, char **argv)
 
 	/* set random seed */
 	gettimeofday(&tv1, NULL);
-	srand((unsigned int) tv1.tv_usec);
+	srandom((unsigned int) tv1.tv_usec);
 
 	/* get start up time */
 	gettimeofday(&tv1, NULL);
