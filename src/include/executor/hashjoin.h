a51a7234fe8eef2abb3bc17cb39e7517a195a5af
Tom Lane <tgl@postgresql.org>
2007-01-28 23:21:26 +0000

    Improve hash join to discard input tuples immediately if they can't match because they contain a null join key (and the join operator is known strict).  Improves performance significantly when the inner relation contains a lot of nulls, as per bug #2930.

--- src/include/executor/hashjoin.h
+++ src/include/executor/hashjoin.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2007, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/executor/hashjoin.h,v 1.41 2006/07/13 18:01:02 momjian Exp $
+ * $PostgreSQL: pgsql/src/include/executor/hashjoin.h,v 1.42 2007/01/05 22:19:54 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -107,6 +107,8 @@ typedef struct HashJoinTableData
 	 * array of the same length as the number of hash keys.
 	 */
 	FmgrInfo   *hashfunctions;	/* lookup data for hash functions */
+
+	bool	   *hashStrict;		/* is each hash join operator strict? */
 
 	Size		spaceUsed;		/* memory space currently used by tuples */
 	Size		spaceAllowed;	/* upper limit for space used */
