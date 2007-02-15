6f7c25fe2aa7bd32d4d70f3c2ad38e263a919d08
Tom Lane <tgl@postgresql.org>
2007-02-15 03:07:21 +0000

    Repair oversight in 8.2 change that improved the handling of "pseudoconstant" WHERE clauses.  createplan.c is now willing to stick a gating Result node almost anywhere in the plan tree, and in particular one can wind up directly underneath a MergeJoin node.  This means it had better be willing to handle Mark/Restore.  Fortunately, that's trivial in such cases, since we can just pass off the call to the input node (which the planner has previously ensured can handle Mark/Restore).  Per report from Phil Frost.

--- src/backend/executor/execAmi.c
+++ src/backend/executor/execAmi.c
@@ -6,7 +6,7 @@
  * Portions Copyright (c) 1996-2006, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- *	$PostgreSQL: pgsql/src/backend/executor/execAmi.c,v 1.88 2006/07/14 14:52:18 momjian Exp $
+ *	$PostgreSQL: pgsql/src/backend/executor/execAmi.c,v 1.89 2006/08/02 01:59:45 joe Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -243,6 +243,10 @@ ExecMarkPos(PlanState *node)
 			ExecSortMarkPos((SortState *) node);
 			break;
 
+		case T_ResultState:
+			ExecResultMarkPos((ResultState *) node);
+			break;
+
 		default:
 			/* don't make hard error unless caller asks to restore... */
 			elog(DEBUG2, "unrecognized node type: %d", (int) nodeTag(node));
@@ -296,6 +300,10 @@ ExecRestrPos(PlanState *node)
 			ExecSortRestrPos((SortState *) node);
 			break;
 
+		case T_ResultState:
+			ExecResultRestrPos((ResultState *) node);
+			break;
+
 		default:
 			elog(ERROR, "unrecognized node type: %d", (int) nodeTag(node));
 			break;
@@ -327,6 +335,16 @@ ExecSupportsMarkRestore(NodeTag plantype)
 		case T_Material:
 		case T_Sort:
 			return true;
+
+		case T_Result:
+			/*
+			 * T_Result only supports mark/restore if it has a child plan
+			 * that does, so we do not have enough information to give a
+			 * really correct answer.  However, for current uses it's
+			 * enough to always say "false", because this routine is not
+			 * asked about gating Result plans, only base-case Results.
+			 */
+			return false;
 
 		default:
 			break;
--- src/include/executor/nodeResult.h
+++ src/include/executor/nodeResult.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2006, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/executor/nodeResult.h,v 1.21 2006/02/28 04:10:28 tgl Exp $
+ * $PostgreSQL: pgsql/src/include/executor/nodeResult.h,v 1.22 2006/03/05 15:58:56 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -20,6 +20,8 @@ extern int	ExecCountSlotsResult(Result *node);
 extern ResultState *ExecInitResult(Result *node, EState *estate, int eflags);
 extern TupleTableSlot *ExecResult(ResultState *node);
 extern void ExecEndResult(ResultState *node);
+extern void ExecResultMarkPos(ResultState *node);
+extern void ExecResultRestrPos(ResultState *node);
 extern void ExecReScanResult(ResultState *node, ExprContext *exprCtxt);
 
 #endif   /* NODERESULT_H */
