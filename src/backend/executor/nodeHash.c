5841ee1156daf0f5bdb520e02d96aaeeff9e06ad
Tom Lane <tgl@postgresql.org>
2007-02-22 22:49:27 +0000

    Fix bug I introduced in recent patch to make hash joins discard null tuples immediately: ExecHashGetHashValue failed to restore the caller's memory context before taking the failure exit.

--- src/backend/executor/nodeHash.c
+++ src/backend/executor/nodeHash.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/executor/nodeHash.c,v 1.109 2007/01/28 23:21:26 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/backend/executor/nodeHash.c,v 1.110 2007/01/30 01:33:36 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -731,7 +731,10 @@ ExecHashGetHashValue(HashJoinTable hashtable,
 		if (isNull)
 		{
 			if (hashtable->hashStrict[i] && !keep_nulls)
-				return false;	/* cannot match */
+			{
+				MemoryContextSwitchTo(oldContext);
+				return false;							/* cannot match */
+			}
 			/* else, leave hashkey unmodified, equivalent to hashcode 0 */
 		}
 		else
