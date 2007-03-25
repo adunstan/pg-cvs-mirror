1aeb67b0babbe6b7a952a226a09815880e5b1c37
Tom Lane <tgl@postgresql.org>
2007-03-25 19:45:14 +0000

    Clean up the representation of special snapshots by including a "method pointer" in every Snapshot struct.  This allows removal of the case-by-case tests in HeapTupleSatisfiesVisibility, which should make it a bit faster (I didn't try any performance tests though).  More importantly, we are no longer violating portable C practices by assuming that small integers are distinct from all pointer values, and HeapTupleSatisfiesDirty no longer has a non-reentrant API involving side-effects on a global variable.
    There were a couple of places calling HeapTupleSatisfiesXXX routines
    directly rather than through the HeapTupleSatisfiesVisibility macro.
    Since these places had to be changed anyway, I chose to make them go
    through the macro for uniformity.
    
    Along the way I renamed HeapTupleSatisfiesSnapshot to HeapTupleSatisfiesMVCC
    to emphasize that it's only used with MVCC-type snapshots.  I was sorely
    tempted to rename HeapTupleSatisfiesVisibility to HeapTupleSatisfiesSnapshot,
    but forebore for the moment to avoid confusion and reduce the likelihood that
    this patch breaks some of the pending patches.  Might want to reconsider
    doing that later.

--- src/backend/catalog/catalog.c
+++ src/backend/catalog/catalog.c
@@ -10,7 +10,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/catalog/catalog.c,v 1.68 2006/10/04 00:29:50 momjian Exp $
+ *	  $PostgreSQL: pgsql/src/backend/catalog/catalog.c,v 1.69 2007/01/05 22:19:24 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -361,9 +361,12 @@ Oid
 GetNewOidWithIndex(Relation relation, Relation indexrel)
 {
 	Oid			newOid;
+	SnapshotData SnapshotDirty;
 	IndexScanDesc scan;
 	ScanKeyData key;
 	bool		collides;
+
+	InitDirtySnapshot(SnapshotDirty);
 
 	/* Generate new OIDs until we find one not in the table */
 	do
@@ -377,7 +380,7 @@ GetNewOidWithIndex(Relation relation, Relation indexrel)
 
 		/* see notes above about using SnapshotDirty */
 		scan = index_beginscan(relation, indexrel,
-							   SnapshotDirty, 1, &key);
+							   &SnapshotDirty, 1, &key);
 
 		collides = HeapTupleIsValid(index_getnext(scan, ForwardScanDirection));
 
