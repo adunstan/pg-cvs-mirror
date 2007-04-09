049043806a0eaa5d77f9c6aaeccdc001e9edb3aa
Magnus Hagander <mha@postgresql.org>
2007-04-09 18:21:22 +0000

    Add cancel handlers so it's possible to Ctrl-C clusterdb, reindexdb and vacuumdb. ITAGAKI Takahiro, with minor fixes from me.

--- src/bin/scripts/common.h
+++ src/bin/scripts/common.h
@@ -4,7 +4,7 @@
  *
  *	Copyright (c) 2003-2007, PostgreSQL Global Development Group
  *
- *	$PostgreSQL: pgsql/src/bin/scripts/common.h,v 1.15 2006/09/22 18:50:41 petere Exp $
+ *	$PostgreSQL: pgsql/src/bin/scripts/common.h,v 1.16 2007/01/05 22:19:50 momjian Exp $
  */
 #ifndef COMMON_H
 #define COMMON_H
@@ -35,6 +35,11 @@ extern PGresult *executeQuery(PGconn *conn, const char *query,
 extern void executeCommand(PGconn *conn, const char *query,
 			   const char *progname, bool echo);
 
+extern bool executeMaintenanceCommand(PGconn *conn, const char *query,
+							   bool echo);
+
 extern bool yesno_prompt(const char *question);
+
+extern void setup_cancel_handler(void);
 
 #endif   /* COMMON_H */
