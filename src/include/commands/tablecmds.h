643bbcd2d42895afe8fd32325d57bd4841bf4d02
Tom Lane <tgl@postgresql.org>
2007-05-11 20:17:15 +0000

    Fix my oversight in enabling domains-of-domains: ALTER DOMAIN ADD CONSTRAINT needs to check the new constraint against columns of derived domains too.
    Also, make it error out if the domain to be modified is used within any
    composite-type columns.  Eventually we should support that case, but it seems
    a bit painful, and not suitable for a back-patch.  For the moment just let the
    user know we can't do it.
    
    Backpatch to 8.2, which is the only released version that allows nested
    domains.  Possibly the other part should be back-patched further.

--- src/include/commands/tablecmds.h
+++ src/include/commands/tablecmds.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2007, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/commands/tablecmds.h,v 1.31 2006/10/04 00:30:08 momjian Exp $
+ * $PostgreSQL: pgsql/src/include/commands/tablecmds.h,v 1.32 2007/01/05 22:19:54 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -44,6 +44,10 @@ extern void renameatt(Oid myrelid,
 
 extern void renamerel(Oid myrelid,
 		  const char *newrelname);
+
+extern void find_composite_type_dependencies(Oid typeOid,
+											 const char *origTblName,
+											 const char *origTypeName);
 
 extern AttrNumber *varattnos_map(TupleDesc old, TupleDesc new);
 extern AttrNumber *varattnos_map_schema(TupleDesc old, List *schema);
