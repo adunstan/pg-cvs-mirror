4c3fc87b7ad0bbe6d3c19cf2cda210a8aa37b09f
Alvaro Herrera <alvherre@postgresql.org>
2007-06-25 17:12:07 +0000

    Remove unused BAD_LOCATION definition.

--- src/include/storage/shmem.h
+++ src/include/storage/shmem.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2007, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/storage/shmem.h,v 1.49 2006/10/15 22:04:08 tgl Exp $
+ * $PostgreSQL: pgsql/src/include/storage/shmem.h,v 1.50 2007/01/05 22:19:58 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -28,7 +28,6 @@
 typedef unsigned long SHMEM_OFFSET;
 
 #define INVALID_OFFSET	(-1)
-#define BAD_LOCATION	(-1)
 
 /*
  * Start of the primary shared memory region, in this process' address space.
