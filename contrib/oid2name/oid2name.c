b6e3286df35cdf6d83355006835e1b198646e5af
Tom Lane <tgl@postgresql.org>
2007-07-15 22:54:21 +0000

    Change a couple of exit(0) to return 0 to suppress complaints from not-too-bright compilers.  Per buildfarm results.

--- contrib/oid2name/oid2name.c
+++ contrib/oid2name/oid2name.c
@@ -4,7 +4,7 @@
  *
  * Originally by
  * B. Palmer, bpalmer@crimelabs.net 1-17-2001
- * $PostgreSQL: pgsql/contrib/oid2name/oid2name.c,v 1.29 2006/03/11 04:38:30 momjian Exp $
+ * $PostgreSQL: pgsql/contrib/oid2name/oid2name.c,v 1.30 2006/10/19 20:38:48 tgl Exp $
  */
 #include "postgres_fe.h"
 
@@ -575,5 +575,5 @@ main(int argc, char **argv)
 	sql_exec_dumpalldbs(pgconn, my_opts);
 
 	PQfinish(pgconn);
-	exit(0);
+	return 0;
 }
