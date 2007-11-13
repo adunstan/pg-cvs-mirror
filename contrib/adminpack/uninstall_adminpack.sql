e31bf76e327c3381f5f19a944e3c08f74af863d5
Bruce Momjian <momjian@postgresql.org>
2007-11-13 04:24:29 +0000

    Add CVS version labels to all install/uninstall scripts.

--- contrib/adminpack/uninstall_adminpack.sql
+++ contrib/adminpack/uninstall_adminpack.sql
@@ -1,3 +1,5 @@
+/* $PostgreSQL: pgsql/contrib/uuid-ossp/uuid-ossp.sql.in,v 1.4 2007/11/13 00:20:36 tgl Exp $ */
+
 DROP FUNCTION pg_catalog.pg_file_write(text, text, bool) ;
 DROP FUNCTION pg_catalog.pg_file_rename(text, text, text) ;
 DROP FUNCTION pg_catalog.pg_file_rename(text, text) ;
