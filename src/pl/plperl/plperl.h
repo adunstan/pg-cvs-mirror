2020ce16da7bd4ffd09cef4f24b5985467aded5b
Tom Lane <tgl@postgresql.org>
2007-11-22 17:47:32 +0000

    Prevent Perl from introducing a possibly-incompatible definition of type "bool" into plperl.c.  This has always been a hazard since Perl allows a platform-specific choice to define bool as int rather than char, but evidently this didn't happen on any platform we support ... until OS X 10.5. Per report from Brandon Maust.
    Back-patch as far as 8.0 --- a bit arbitrary, but it seems unlikely anyone
    will be trying to port 7.x onto new platforms.

--- src/pl/plperl/plperl.h
+++ src/pl/plperl/plperl.h
@@ -8,7 +8,7 @@
  * Portions Copyright (c) 1996-2006, PostgreSQL Global Development Group
  * Portions Copyright (c) 1995, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/pl/plperl/plperl.h,v 1.4 2006/03/05 16:40:51 adunstan Exp $
+ * $PostgreSQL: pgsql/src/pl/plperl/plperl.h,v 1.5 2006/10/04 00:30:13 momjian Exp $
  */
 
 #ifndef PL_PERL_H
@@ -36,6 +36,11 @@
 #ifndef pTHX_
 #define pTHX_
 #define pTHX void
+#endif
+
+/* perl may have a different width of "bool", don't buy it */
+#ifdef bool
+#undef bool
 #endif
 
 /* routines from spi_internal.c */
