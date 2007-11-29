48bc755ff8cab5553912e636725063d4d4a028f8
Magnus Hagander <mha@postgresql.org>
2007-11-29 16:44:26 +0000

    Back-patch mingw configure-check for gettimeofday so that 8.2 can be built with current versions of mingw.

--- src/include/port.h
+++ src/include/port.h
@@ -6,7 +6,7 @@
  * Portions Copyright (c) 1996-2006, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/port.h,v 1.106 2006/11/28 01:12:33 adunstan Exp $
+ * $PostgreSQL: pgsql/src/include/port.h,v 1.106.2.1 2007/01/11 02:40:12 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -288,8 +288,11 @@ extern FILE *pgwin32_fopen(const char *, const char *);
 extern long lrand48(void);
 extern void srand48(long seed);
 
+/* New versions of MingW have gettimeofday, old mingw and msvc don't */
+#ifndef HAVE_GETTIMEOFDAY
 /* Last parameter not used */
 extern int	gettimeofday(struct timeval * tp, struct timezone * tzp);
+#endif
 #else							/* !WIN32 */
 
 /*
