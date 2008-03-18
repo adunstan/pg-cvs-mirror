a92c1e3d80353120cd0b26ae7b3d44fc05764961
Tom Lane <tgl@postgresql.org>
2008-03-18 22:04:14 +0000

    Arrange to "inline" SQL functions that appear in a query's FROM clause, are declared to return set, and consist of just a single SELECT.  We can replace the FROM-item with a sub-SELECT and then optimize much as if we were dealing with a view.  Patch from Richard Rowell, cleaned up by me.

--- src/include/optimizer/clauses.h
+++ src/include/optimizer/clauses.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2008, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/optimizer/clauses.h,v 1.87 2007/02/19 07:03:34 tgl Exp $
+ * $PostgreSQL: pgsql/src/include/optimizer/clauses.h,v 1.88 2008/01/01 19:45:58 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -78,6 +78,8 @@ extern void set_coercionform_dontcare(Node *node);
 extern Node *eval_const_expressions(Node *node);
 
 extern Node *estimate_expression_value(PlannerInfo *root, Node *node);
+
+extern Query *inline_set_returning_function(PlannerInfo *root, Node *node);
 
 extern bool expression_tree_walker(Node *node, bool (*walker) (),
 											   void *context);
