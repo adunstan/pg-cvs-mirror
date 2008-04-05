9865d0a8c7fb6f428af66348e059431771b4ea0b
Tom Lane <tgl@postgresql.org>
2008-04-05 01:58:28 +0000

    Defend against JOINs having more than 32K columns altogether.  We cannot currently support this because we must be able to build Vars referencing join columns, and varattno is only 16 bits wide.  Perhaps this should be improved in future, but considering that it never came up before, I'm not sure the problem is worth much effort.  Per bug #4070 from Marcello Ceschia.
    The problem seems largely academic in 8.0 and 7.4, because they have
    (different) O(N^2) performance issues with such wide joins, but
    back-patch all the way anyway.

--- src/backend/parser/parse_relation.c
+++ src/backend/parser/parse_relation.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/parser/parse_relation.c,v 1.129 2007/11/11 19:22:49 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/backend/parser/parse_relation.c,v 1.130 2008/01/01 19:45:51 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -1024,6 +1024,16 @@ addRangeTableEntryForJoin(ParseState *pstate,
 	RangeTblEntry *rte = makeNode(RangeTblEntry);
 	Alias	   *eref;
 	int			numaliases;
+
+	/*
+	 * Fail if join has too many columns --- we must be able to reference
+	 * any of the columns with an AttrNumber.
+	 */
+	if (list_length(aliasvars) > MaxAttrNumber)
+		ereport(ERROR,
+				(errcode(ERRCODE_PROGRAM_LIMIT_EXCEEDED),
+				 errmsg("joins can have at most %d columns",
+						MaxAttrNumber)));
 
 	rte->rtekind = RTE_JOIN;
 	rte->relid = InvalidOid;
