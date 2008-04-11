9664859276308d31ba42a13c75a94d8ff31cb47b
Tom Lane <tgl@postgresql.org>
2008-04-11 22:53:24 +0000

    Fix several datatype input functions that were allowing unused bytes in their results to contain uninitialized, unpredictable values.  While this was okay as far as the datatypes themselves were concerned, it's a problem for the parser because occurrences of the "same" literal might not be recognized as equal by datumIsEqual (and hence not by equal()).  It seems sufficient to fix this in the input functions since the only critical use of equal() is in the parser's comparisons of ORDER BY and DISTINCT expressions. Per a trouble report from Marc Cousin.
    Patch all the way back.  Interestingly, array_in did not have the bug before
    8.2, which may explain why the issue went unnoticed for so long.

--- src/backend/utils/adt/geo_ops.c
+++ src/backend/utils/adt/geo_ops.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/utils/adt/geo_ops.c,v 1.88 2004/12/31 22:01:21 pgsql Exp $
+ *	  $PostgreSQL: pgsql/src/backend/utils/adt/geo_ops.c,v 1.88.4.1 2007/12/18 00:04:29 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -1375,6 +1375,8 @@ path_in(PG_FUNCTION_ARGS)
 			 errmsg("invalid input syntax for type path: \"%s\"", str)));
 
 	path->closed = (!isopen);
+	/* prevent instability in unused pad bytes */
+	path->dummy = 0;
 
 	PG_RETURN_PATH_P(path);
 }
