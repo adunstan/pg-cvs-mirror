2868af9d043b3464e3d1605d4b0712ba048d7f44
Bruce Momjian <momjian@postgresql.org>
2008-06-26 18:25:24 +0000

    Fix pg_ctl bug where detection of binary location from postmaster.opts wasn't working.

--- src/bin/pg_ctl/pg_ctl.c
+++ src/bin/pg_ctl/pg_ctl.c
@@ -4,7 +4,7 @@
  *
  * Portions Copyright (c) 1996-2008, PostgreSQL Global Development Group
  *
- * $PostgreSQL: pgsql/src/bin/pg_ctl/pg_ctl.c,v 1.101 2008/06/26 02:47:19 momjian Exp $
+ * $PostgreSQL: pgsql/src/bin/pg_ctl/pg_ctl.c,v 1.102 2008/06/26 03:51:56 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -610,7 +610,7 @@ read_post_opts(void)
 					*arg1 = '\0';	/* terminate so we get only program name */
 					post_opts = arg1 + 1; /* point past whitespace */
 				}
-				if (postgres_path != NULL)
+				if (postgres_path == NULL)
 					postgres_path = optline;
 			}
 		}
