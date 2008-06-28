7be946cc83aae9611713f7a15cbf085400017926
Tom Lane <tgl@postgresql.org>
2008-06-28 16:45:22 +0000

    If pnstrdup is going to be promoted to a generally available function, it ought to conform to the rest of palloc.h in using Size for sizes.

--- src/backend/utils/mmgr/mcxt.c
+++ src/backend/utils/mmgr/mcxt.c
@@ -14,7 +14,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/utils/mmgr/mcxt.c,v 1.63 2008/01/01 19:45:55 momjian Exp $
+ *	  $PostgreSQL: pgsql/src/backend/utils/mmgr/mcxt.c,v 1.64 2008/06/18 18:42:54 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -624,18 +624,6 @@ repalloc(void *pointer, Size size)
 												 pointer, size);
 }
 
-/* Like pstrdup(), but append null byte */
-char *
-pnstrdup(const char *in, int len)
-{
-	char	   *out = palloc(len + 1);
-
-	memcpy(out, in, len);
-	out[len] = '\0';
-	return out;
-}
-
-
 /*
  * MemoryContextSwitchTo
  *		Returns the current context; installs the given context.
@@ -674,6 +662,21 @@ MemoryContextStrdup(MemoryContext context, const char *string)
 	memcpy(nstr, string, len);
 
 	return nstr;
+}
+
+/*
+ * pnstrdup
+ *		Like pstrdup(), but append null byte to a
+ *		not-necessarily-null-terminated input string.
+ */
+char *
+pnstrdup(const char *in, Size len)
+{
+	char	   *out = palloc(len + 1);
+
+	memcpy(out, in, len);
+	out[len] = '\0';
+	return out;
 }
 
 
