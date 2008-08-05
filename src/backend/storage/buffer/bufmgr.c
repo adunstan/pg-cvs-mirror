a9f7b9836380aa7f37a811d69e16bb5c801c2c5b
Tom Lane <tgl@postgresql.org>
2008-08-05 15:09:04 +0000

    In ReadOrZeroBuffer (and related entry points), don't bother to call PageHeaderIsValid when we zero the buffer instead of reading the page in. The actual performance improvement is probably marginal since this function isn't very heavily used, but a cycle saved is a cycle earned.
    Zdenek Kotala

--- src/backend/storage/buffer/bufmgr.c
+++ src/backend/storage/buffer/bufmgr.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/storage/buffer/bufmgr.c,v 1.234 2008/07/13 20:45:47 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/backend/storage/buffer/bufmgr.c,v 1.235 2008/08/01 13:16:08 alvherre Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -352,28 +352,31 @@ ReadBuffer_common(SMgrRelation smgr, bool isLocalBuf, BlockNumber blockNum,
 		if (zeroPage)
 			MemSet((char *) bufBlock, 0, BLCKSZ);
 		else
-			smgrread(smgr, blockNum, (char *) bufBlock);
-		/* check for garbage data */
-		if (!PageHeaderIsValid((PageHeader) bufBlock))
 		{
-			if (zero_damaged_pages)
+			smgrread(smgr, blockNum, (char *) bufBlock);
+
+			/* check for garbage data */
+			if (!PageHeaderIsValid((PageHeader) bufBlock))
 			{
-				ereport(WARNING,
-						(errcode(ERRCODE_DATA_CORRUPTED),
-						 errmsg("invalid page header in block %u of relation %u/%u/%u; zeroing out page",
-								blockNum, 
-								smgr->smgr_rnode.spcNode,
-								smgr->smgr_rnode.dbNode,
-								smgr->smgr_rnode.relNode)));
-				MemSet((char *) bufBlock, 0, BLCKSZ);
+				if (zero_damaged_pages)
+				{
+					ereport(WARNING,
+							(errcode(ERRCODE_DATA_CORRUPTED),
+							 errmsg("invalid page header in block %u of relation %u/%u/%u; zeroing out page",
+									blockNum, 
+									smgr->smgr_rnode.spcNode,
+									smgr->smgr_rnode.dbNode,
+									smgr->smgr_rnode.relNode)));
+					MemSet((char *) bufBlock, 0, BLCKSZ);
+				}
+				else
+					ereport(ERROR,
+							(errcode(ERRCODE_DATA_CORRUPTED),
+							 errmsg("invalid page header in block %u of relation %u/%u/%u",
+									blockNum, smgr->smgr_rnode.spcNode,
+									smgr->smgr_rnode.dbNode,
+									smgr->smgr_rnode.relNode)));
 			}
-			else
-				ereport(ERROR,
-						(errcode(ERRCODE_DATA_CORRUPTED),
-				 errmsg("invalid page header in block %u of relation %u/%u/%u",
-						blockNum, smgr->smgr_rnode.spcNode,
-						smgr->smgr_rnode.dbNode,
-						smgr->smgr_rnode.relNode)));
 		}
 	}
 
