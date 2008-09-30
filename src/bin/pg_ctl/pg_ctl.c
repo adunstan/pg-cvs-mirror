2f98c01404bcb3d6946142133e30846acf4b9351
Tom Lane <tgl@postgresql.org>
2008-09-30 13:14:07 +0000

    Recent patches to pg_ctl broke "pg_ctl restart" for the case where no command-line options had been given to the postmaster; and just plain broke it altogether in 8.1 and 8.0.  Per report from KaiGai Kohei.

--- src/bin/pg_ctl/pg_ctl.c
+++ src/bin/pg_ctl/pg_ctl.c
@@ -4,7 +4,7 @@
  *
  * Portions Copyright (c) 1996-2008, PostgreSQL Global Development Group
  *
- * $PostgreSQL: pgsql/src/bin/pg_ctl/pg_ctl.c,v 1.92.2.3 2008/02/29 23:31:42 adunstan Exp $
+ * $PostgreSQL: pgsql/src/bin/pg_ctl/pg_ctl.c,v 1.92.2.4 2008/06/27 01:53:31 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -624,6 +624,8 @@ read_post_opts(void)
 					*arg1 = '\0';	/* terminate so we get only program name */
 					post_opts = arg1 + 1; /* point past whitespace */
 				}
+				else
+					post_opts = "";
 				if (postgres_path == NULL)
 					postgres_path = optline;
 			}
