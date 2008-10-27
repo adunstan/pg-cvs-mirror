1ae59718b84c1ea9864aa97221981ed91f1cf49f
Tom Lane <tgl@postgresql.org>
2008-10-27 19:37:48 +0000

    Install a more robust solution for the problem of infinite error-processing recursion when we are unable to convert a localized error message to the client's encoding.  We've been over this ground before, but as reported by Ibrar Ahmed, it still didn't work in the case of conversion failures for the conversion-failure message itself :-(.  Fix by installing a "circuit breaker" that disables attempts to localize this message once we get into recursion trouble.
    Patch all supported branches, because it is in fact broken in all of them;
    though I had to add some missing translations to the older branches in
    order to expose the failure in the particular test case I was using.

--- src/include/utils/elog.h
+++ src/include/utils/elog.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2005, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/utils/elog.h,v 1.77 2004/09/16 21:59:17 tgl Exp $
+ * $PostgreSQL: pgsql/src/include/utils/elog.h,v 1.78 2004/12/31 22:03:46 pgsql Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -282,6 +282,7 @@ extern int	Log_destination;
 
 /* Other exported functions */
 extern void DebugFileOpen(void);
+extern bool in_error_recursion_trouble(void);
 
 /*
  * Write errors to stderr (or by equal means when stderr is
