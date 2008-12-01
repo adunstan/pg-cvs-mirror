bbc4b259702e6289b774cbaa738161258158c1eb
Tom Lane <tgl@postgresql.org>
2008-12-01 17:06:27 +0000

    Ensure that the contents of a holdable cursor don't depend on out-of-line toasted values, since those could get dropped once the cursor's transaction is over.  Per bug #4553 from Andrew Gierth.
    Back-patch as far as 8.1.  The bug actually exists back to 7.4 when holdable
    cursors were introduced, but this patch won't work before 8.1 without
    significant adjustments.  Given the lack of field complaints, it doesn't seem
    worth the work (and risk of introducing new bugs) to try to make a patch for
    the older branches.

--- src/include/executor/tstoreReceiver.h
+++ src/include/executor/tstoreReceiver.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2008, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/executor/tstoreReceiver.h,v 1.9 2007/01/05 22:19:55 momjian Exp $
+ * $PostgreSQL: pgsql/src/include/executor/tstoreReceiver.h,v 1.10 2008/01/01 19:45:57 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -21,5 +21,8 @@
 
 extern DestReceiver *CreateTuplestoreDestReceiver(Tuplestorestate *tStore,
 							 MemoryContext tContext);
+
+extern void SetTuplestoreDestReceiverDeToast(DestReceiver *self,
+											 bool detoast);
 
 #endif   /* TSTORE_RECEIVER_H */
