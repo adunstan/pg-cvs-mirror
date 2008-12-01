8f6dcf8c227c347ce6f517bf6aa0556bc4fbc2db
Tom Lane <tgl@postgresql.org>
2008-12-01 21:06:13 +0000

    Fix an oversight in the code that makes transitive-equality deductions from outer join clauses.  Given, say, 	... from a left join b on a.a1 = b.b1 where a.a1 = 42; we'll deduce a clause b.b1 = 42 and then mark the original join clause redundant (we can't remove it completely for reasons I don't feel like squeezing into this log entry).  However the original implementation of that wasn't bulletproof, because clause_selectivity() wouldn't honor this_selec if given nonzero varRelid --- which in practice meant that it worked as desired *except* when considering index scan quals.  Which resulted in bogus underestimation of the size of the indexscan result for an inner indexscan in an outer join, and consequently a possibly bad choice of indexscan vs. bitmap scan.  Fix by introducing an explicit test into clause_selectivity().  Also, to make sure we don't trigger that test in corner cases, change the convention to be that this_selec > 1, not this_selec = 1, means it's been marked redundant.  Per trouble report from Scara Maccai.
    Back-patch to 8.2, where the problem was introduced.

--- src/backend/optimizer/path/clausesel.c
+++ src/backend/optimizer/path/clausesel.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/optimizer/path/clausesel.c,v 1.93 2008/08/22 00:16:03 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/backend/optimizer/path/clausesel.c,v 1.94 2008/10/04 21:56:53 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -512,6 +512,12 @@ clause_selectivity(PlannerInfo *root,
 			if (!IsA(rinfo->clause, Const))
 				return (Selectivity) 1.0;
 		}
+
+		/*
+		 * If the clause is marked redundant, always return 1.0.
+		 */
+		if (rinfo->this_selec > 1)
+			return (Selectivity) 1.0;
 
 		/*
 		 * If possible, cache the result of the selectivity calculation for
