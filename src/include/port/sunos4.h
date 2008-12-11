fb22658cff764110b28f357dbf4e157365901e7b
Peter Eisentraut <petere@postgresql.org>
2008-12-11 09:17:07 +0000

    Move carefully obscured SunOS 4 specific #include out of c.h into port header file.  SunOS 4 is probably broken anyway, but this item stuck out as completely weird.

--- src/include/port/sunos4.h
+++ src/include/port/sunos4.h
@@ -1,5 +1,7 @@
 /*
- * $PostgreSQL:$ 
+ * $PostgreSQL: pgsql/src/include/port/sunos4.h,v 1.10 2008/05/17 01:28:24 adunstan Exp $ 
  *
  * sprintf() returns char *, not int, on SunOS 4.1.x */
 #define SPRINTF_CHAR
+
+#include <unistd.h>
