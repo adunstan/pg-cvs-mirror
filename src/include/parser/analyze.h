886c2a03589e6ec6494259e81a8ec1d7e93d7090
Tom Lane <tgl@postgresql.org>
2008-12-13 02:00:53 +0000

    Fix failure to ensure that a snapshot is available to datatype input functions when they are invoked by the parser.  We had been setting up a snapshot at plan time but really it needs to be done earlier, before parse analysis. Per report from Dmitry Koterov.
    Also fix two related problems discovered while poking at this one:
    exec_bind_message called datatype input functions without establishing a
    snapshot, and SET CONSTRAINTS IMMEDIATE could call trigger functions without
    establishing a snapshot.
    
    Backpatch to 8.2.  The underlying problem goes much further back, but it is
    masked in 8.1 and before because we didn't attempt to invoke domain check
    constraints within datatype input.  It would only be exposed if a C-language
    datatype input function used the snapshot; which evidently none do, or we'd
    have heard complaints sooner.  Since this code has changed a lot over time,
    a back-patch is hardly risk-free, and so I'm disinclined to patch further
    than absolutely necessary.

--- src/include/parser/analyze.h
+++ src/include/parser/analyze.h
@@ -6,7 +6,7 @@
  * Portions Copyright (c) 1996-2006, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/parser/analyze.h,v 1.33 2006/04/30 18:30:40 tgl Exp $
+ * $PostgreSQL: pgsql/src/include/parser/analyze.h,v 1.34 2006/10/04 00:30:09 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -21,6 +21,7 @@ extern List *parse_analyze(Node *parseTree, const char *sourceText,
 extern List *parse_analyze_varparams(Node *parseTree, const char *sourceText,
 						Oid **paramTypes, int *numParams);
 extern List *parse_sub_analyze(Node *parseTree, ParseState *parentParseState);
+extern bool analyze_requires_snapshot(Node *parseTree);
 extern List *analyzeCreateSchemaStmt(CreateSchemaStmt *stmt);
 extern void CheckSelectLocking(Query *qry);
 extern void applyLockingClause(Query *qry, Index rtindex,
