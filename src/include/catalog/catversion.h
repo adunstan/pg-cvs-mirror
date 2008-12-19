c0680fc965c702b9a99bd41f0335f041f2b42771
Tom Lane <tgl@postgresql.org>
2008-12-19 18:25:20 +0000

    Add a new column proiswindow to pg_proc.  It doesn't actually do anything useful yet, but I'm tired of re-merging this aspect of the window functions patch.

--- src/include/catalog/catversion.h
+++ src/include/catalog/catversion.h
@@ -37,7 +37,7 @@
  * Portions Copyright (c) 1996-2008, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/catalog/catversion.h,v 1.511 2008/12/04 17:51:27 petere Exp $
+ * $PostgreSQL: pgsql/src/include/catalog/catversion.h,v 1.512 2008/12/19 16:25:18 petere Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -53,6 +53,6 @@
  */
 
 /*							yyyymmddN */
-#define CATALOG_VERSION_NO	200812191
+#define CATALOG_VERSION_NO	200812192
 
 #endif
