976988b0cf3b285f1e637d0afb2f3374c591fc07
Magnus Hagander <mha@postgresql.org>
2009-01-27 12:46:16 +0000

    Silence compiler warning on win32.
    ITAGAKI Takahiro

--- src/test/regress/pg_regress.c
+++ src/test/regress/pg_regress.c
@@ -11,7 +11,7 @@
  * Portions Copyright (c) 1996-2009, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/test/regress/pg_regress.c,v 1.56 2009/01/01 17:24:04 momjian Exp $
+ * $PostgreSQL: pgsql/src/test/regress/pg_regress.c,v 1.57 2009/01/08 20:09:06 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -1364,7 +1364,7 @@ wait_for_tests(PID_TYPE *pids, int *statuses, char **names, int num_tests)
 			if (p == pids[i])
 			{
 #ifdef WIN32
-				GetExitCodeProcess(pids[i], &exit_status);
+				GetExitCodeProcess(pids[i], (LPDWORD) &exit_status);
 				CloseHandle(pids[i]);
 #endif
 				pids[i] = INVALID_PID;
