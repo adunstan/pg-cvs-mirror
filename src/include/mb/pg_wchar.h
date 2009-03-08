f4892b3880d48f27dfa0a8852bebae9550d47661
Alvaro Herrera <alvherre@postgresql.org>
2009-03-08 18:10:17 +0000

    pg_bind_textdomain_codeset must exist only on ENABLE_NLS.

--- src/include/mb/pg_wchar.h
+++ src/include/mb/pg_wchar.h
@@ -6,7 +6,7 @@
  * Portions Copyright (c) 1996-2009, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/mb/pg_wchar.h,v 1.84 2009/02/10 19:29:39 petere Exp $
+ * $PostgreSQL: pgsql/src/include/mb/pg_wchar.h,v 1.85 2009/03/08 16:07:12 alvherre Exp $
  *
  *	NOTES
  *		This is used both by the backend and by libpq, but should not be
@@ -392,7 +392,9 @@ extern const char *pg_get_client_encoding_name(void);
 extern void SetDatabaseEncoding(int encoding);
 extern int	GetDatabaseEncoding(void);
 extern const char *GetDatabaseEncodingName(void);
+#ifdef ENABLE_NLS
 extern void pg_bind_textdomain_codeset(const char *domainname, int encoding);
+#endif
 
 extern int	pg_valid_client_encoding(const char *name);
 extern int	pg_valid_server_encoding(const char *name);
