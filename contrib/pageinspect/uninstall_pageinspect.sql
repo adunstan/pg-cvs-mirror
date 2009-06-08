6bc88b04426fda01b88e0ffdb135a0db54c99590
Tom Lane <tgl@postgresql.org>
2009-06-08 16:22:44 +0000

    Fix contrib/pageinspect to not create an ABI breakage between 8.3 and 8.4. The original implementation of the 3-argument form of get_raw_page() risked core dumps if the 8.3 SQL function definition was mistakenly used with the 8.4 module, which is entirely likely after a dump-and-reload upgrade.  To protect 8.4 beta testers against upgrade problems, add a check on PG_NARGS.
    In passing, fix missed additions to the uninstall script, and polish the
    docs a trifle.

--- contrib/pageinspect/uninstall_pageinspect.sql
+++ contrib/pageinspect/uninstall_pageinspect.sql
@@ -1,11 +1,13 @@
-/* $PostgreSQL: pgsql/contrib/uuid-ossp/uuid-ossp.sql.in,v 1.4 2007/11/13 00:20:36 tgl Exp $ */
+/* $PostgreSQL: pgsql/contrib/pageinspect/uninstall_pageinspect.sql,v 1.4 2007/11/13 04:24:28 momjian Exp $ */
 
 -- Adjust this setting to control where the objects get dropped.
 SET search_path = public;
 
 DROP FUNCTION get_raw_page(text, int4);
+DROP FUNCTION get_raw_page(text, text, int4);
 DROP FUNCTION page_header(bytea);
 DROP FUNCTION heap_page_items(bytea);
 DROP FUNCTION bt_metap(text);
 DROP FUNCTION bt_page_stats(text, int4);
 DROP FUNCTION bt_page_items(text, int4);
+DROP FUNCTION fsm_page_contents(bytea);
