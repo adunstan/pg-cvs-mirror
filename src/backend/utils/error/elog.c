6521395096b06beaafa102fe62faa3cbfff0a938
Tom Lane <tgl@postgresql.org>
2009-06-25 23:07:15 +0000

    Add __attribute__((format_arg(1))) to the declaration of err_gettext(), to restore gcc's ability to crosscheck format arguments within elog.c. Noted in a test compilation with -Wformat-nonliteral enabled.

--- src/backend/utils/error/elog.c
+++ src/backend/utils/error/elog.c
@@ -42,7 +42,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/utils/error/elog.c,v 1.214 2009/06/04 18:33:07 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/backend/utils/error/elog.c,v 1.215 2009/06/11 14:49:05 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -74,6 +74,11 @@
 
 #undef _
 #define _(x) err_gettext(x)
+
+static const char *err_gettext(const char *str)
+/* This extension allows gcc to check the format string for consistency with
+   the supplied arguments. */
+__attribute__((format_arg(1)));
 
 /* Global variables */
 ErrorContextCallback *error_context_stack = NULL;
