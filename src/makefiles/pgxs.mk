f5ba1b9ea56dcbf5ad4b609f95ae22e1ef4a1eb3
Peter Eisentraut <petere@postgresql.org>
2009-07-08 13:47:29 +0000

    When calling unsupported "make check" with a pgxs module, return a nonzero exit code.

--- src/makefiles/pgxs.mk
+++ src/makefiles/pgxs.mk
@@ -1,6 +1,6 @@
 # PGXS: PostgreSQL extensions makefile
 
-# $PostgreSQL: pgsql/src/makefiles/pgxs.mk,v 1.14 2008/10/02 12:25:25 tgl Exp $ 
+# $PostgreSQL: pgsql/src/makefiles/pgxs.mk,v 1.15 2008/10/03 08:00:16 petere Exp $ 
 
 # This file contains generic rules to build many kinds of simple
 # extension modules.  You only need to set a few variables and include
@@ -266,6 +266,7 @@ installcheck: submake
 check:
 	@echo "'make check' is not supported."
 	@echo "Do 'make install', then 'make installcheck' instead."
+	@exit 1
 endif # REGRESS
 
 
