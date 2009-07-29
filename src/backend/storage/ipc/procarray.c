f92f54169cc1aaad98dccc3446eba3d3bcae8769
Tom Lane <tgl@postgresql.org>
2009-07-29 15:57:33 +0000

    Fix a thinko introduced into CountActiveBackends by a recent patch: we should ignore NULL array entries, not non-NULL ones.  This had the effect of disabling commit_delay, and could have caused a crash in the rare race condition the patch was intended to fix.
    Bug report and diagnosis by Jeff Janes, in bug #4952.

--- src/backend/storage/ipc/procarray.c
+++ src/backend/storage/ipc/procarray.c
@@ -23,7 +23,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/storage/ipc/procarray.c,v 1.19 2006/11/05 22:42:09 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/backend/storage/ipc/procarray.c,v 1.19.2.1 2009/03/31 05:18:43 heikki Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -836,7 +836,7 @@ CountActiveBackends(void)
 		 * the free list and are recycled. Its contents are nonsense in that
 		 * case, but that's acceptable for this function.
 		 */
-		if (proc != NULL)
+		if (proc == NULL)
 			continue;
 
 		if (proc == MyProc)
