a641193549c3766dae978f1fb992ff7bf8367ef2
Peter Eisentraut <petere@postgresql.org>
2009-08-07 20:50:22 +0000

    Expand test coverage support to entire tree
    Test coverage support now covers the entire source tree, including
    contrib, instead of just src/backend.  In a related but independent
    development, the commands make coverage and make coverage-html can be run
    in any directory.
    
    This turned out to be much easier than feared.  Besides a few ad hoc fixes
    to pass the make target down the tree, change all affected makefiles to
    list their directories in the SUBDIRS variable, changed from variants like
    DIRS and WANTED_DIRS.  MSVC build fix was attempted as well.

--- src/tools/msvc/Mkvcbuild.pm
+++ src/tools/msvc/Mkvcbuild.pm
@@ -3,7 +3,7 @@ package Mkvcbuild;
 #
 # Package that generates build files for msvc build
 #
-# $PostgreSQL: pgsql/src/tools/msvc/Mkvcbuild.pm,v 1.40 2009/06/05 18:29:56 adunstan Exp $
+# $PostgreSQL: pgsql/src/tools/msvc/Mkvcbuild.pm,v 1.41 2009/07/16 17:43:52 tgl Exp $
 #
 use Carp;
 use Win32;
@@ -321,7 +321,7 @@ sub mkvcbuild
 
     $mf = Project::read_file('src\backend\utils\mb\conversion_procs\Makefile');
     $mf =~ s{\\s*[\r\n]+}{}mg;
-    $mf =~ m{DIRS\s*=\s*(.*)$}m || die 'Could not match in conversion makefile' . "\n";
+    $mf =~ m{SUBDIRS\s*=\s*(.*)$}m || die 'Could not match in conversion makefile' . "\n";
     foreach my $sub (split /\s+/,$1)
     {
         my $mf = Project::read_file('src\backend\utils\mb\conversion_procs\\' . $sub . '\Makefile');
