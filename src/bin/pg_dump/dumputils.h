b3f1608bebe110d44286a47aebf8e0a1c750e401
Alvaro Herrera <alvherre@postgresql.org>
2009-10-07 22:14:26 +0000

    Make it possibly to specify GUC params per user and per database.
    Create a new catalog pg_db_role_setting where they are now stored, and better
    encapsulate the code that deals with settings into its realm.  The old
    datconfig and rolconfig columns are removed.
    
    psql has gained a \drds command to display the settings.
    
    Backwards compatibility warning: while the backwards-compatible system views
    still have the config columns, they no longer completely represent the
    configuration for a user or database.
    
    Catalog version bumped.

--- src/bin/pg_dump/dumputils.h
+++ src/bin/pg_dump/dumputils.h
@@ -8,7 +8,7 @@
  * Portions Copyright (c) 1996-2009, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/bin/pg_dump/dumputils.h,v 1.25 2009/08/04 21:56:08 tgl Exp $
+ * $PostgreSQL: pgsql/src/bin/pg_dump/dumputils.h,v 1.26 2009/10/05 19:24:45 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -40,7 +40,7 @@ extern bool buildDefaultACLCommands(const char *type, const char *nspname,
 						const char *acls, const char *owner,
 						int remoteVersion,
 						PQExpBuffer sql);
-extern void processSQLNamePattern(PGconn *conn, PQExpBuffer buf,
+extern bool processSQLNamePattern(PGconn *conn, PQExpBuffer buf,
 					  const char *pattern,
 					  bool have_where, bool force_escape,
 					  const char *schemavar, const char *namevar,
