560a94a4034de3c17ae0bba9aab75b25f01703ca
Peter Eisentraut <petere@postgresql.org>
2009-10-20 18:23:27 +0000

    Translations update for 8.5alpha2

--- src/bin/pg_controldata/nls.mk
+++ src/bin/pg_controldata/nls.mk
@@ -1,5 +1,5 @@
-# $PostgreSQL: pgsql/src/bin/pg_controldata/nls.mk,v 1.18 2009/04/09 19:38:51 petere Exp $
+# $PostgreSQL: pgsql/src/bin/pg_controldata/nls.mk,v 1.19 2009/06/26 19:33:49 petere Exp $
 CATALOG_NAME	:= pg_controldata
-AVAIL_LANGUAGES	:= de es fr ja ko pt_BR sv ta tr
+AVAIL_LANGUAGES	:= de es fr it ja ko pt_BR sv ta tr
 GETTEXT_FILES	:= pg_controldata.c
 GETTEXT_TRIGGERS:= _
--- src/bin/pg_resetxlog/nls.mk
+++ src/bin/pg_resetxlog/nls.mk
@@ -1,5 +1,5 @@
-# $PostgreSQL: pgsql/src/bin/pg_resetxlog/nls.mk,v 1.19 2009/04/09 19:38:51 petere Exp $
+# $PostgreSQL: pgsql/src/bin/pg_resetxlog/nls.mk,v 1.20 2009/06/26 19:33:50 petere Exp $
 CATALOG_NAME	:= pg_resetxlog
-AVAIL_LANGUAGES	:= de es fr ja ko pt_BR ro ru sv ta tr
+AVAIL_LANGUAGES	:= de es fr it ja ko pt_BR ro ru sv ta tr
 GETTEXT_FILES	:= pg_resetxlog.c
 GETTEXT_TRIGGERS:= _
