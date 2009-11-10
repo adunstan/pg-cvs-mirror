259e1ee65b5c67e103e0dbd7345a30de5e4e4b18
Alvaro Herrera <alvherre@postgresql.org>
2009-11-10 18:01:11 +0000

    Fix longstanding problems in VACUUM caused by untimely interruptions
    In VACUUM FULL, an interrupt after the initial transaction has been recorded
    as committed can cause postmaster to restart with the following error message:
    PANIC: cannot abort transaction NNNN, it was already committed
    This problem has been reported many times.
    
    In lazy VACUUM, an interrupt after the table has been truncated by
    lazy_truncate_heap causes other backends' relcache to still point to the
    removed pages; this can cause future INSERT and UPDATE queries to error out
    with the following error message:
    could not read block XX of relation 1663/NNN/MMMM: read only 0 of 8192 bytes
    The window to this race condition is extremely narrow, but it has been seen in
    the wild involving a cancelled autovacuum process.
    
    The solution for both problems is to inhibit interrupts in both operations
    until after the respective transactions have been committed.  It's not a
    complete solution, because the transaction could theoretically be aborted by
    some other error, but at least fixes the most common causes of both problems.

--- src/include/commands/vacuum.h
+++ src/include/commands/vacuum.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2005, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/commands/vacuum.h,v 1.61 2005/10/03 22:52:24 tgl Exp $
+ * $PostgreSQL: pgsql/src/include/commands/vacuum.h,v 1.62 2005/10/15 02:49:44 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -125,7 +125,7 @@ extern bool vac_is_partial_index(Relation indrel);
 extern void vacuum_delay_point(void);
 
 /* in commands/vacuumlazy.c */
-extern void lazy_vacuum_rel(Relation onerel, VacuumStmt *vacstmt);
+extern bool lazy_vacuum_rel(Relation onerel, VacuumStmt *vacstmt);
 
 /* in commands/analyze.c */
 extern void analyze_rel(Oid relid, VacuumStmt *vacstmt);
