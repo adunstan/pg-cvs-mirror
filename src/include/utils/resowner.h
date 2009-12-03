89a5eead6c03d3d74342e469ebf5d80567d166b1
Heikki Linnakangas <heikki@postgresql.org>
2009-12-03 11:04:03 +0000

    Fix bug in temporary file management with subtransactions. A cursor opened in a subtransaction stays open even if the subtransaction is aborted, so any temporary files related to it must stay alive as well. With the patch, we use ResourceOwners to track open temporary files and don't automatically close them at subtransaction end (though in the normal case temporary files are registered with the subtransaction resource owner and will therefore be closed).
    At end of top transaction, we still check that there's no temporary files
    marked as close-at-end-of-transaction open, but that's now just a debugging
    cross-check as the resource owner cleanup should've closed them already.

--- src/include/utils/resowner.h
+++ src/include/utils/resowner.h
@@ -12,7 +12,7 @@
  * Portions Copyright (c) 1996-2005, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/utils/resowner.h,v 1.4 2004/08/29 05:06:59 momjian Exp $
+ * $PostgreSQL: pgsql/src/include/utils/resowner.h,v 1.5 2004/12/31 22:03:46 pgsql Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -20,6 +20,7 @@
 #define RESOWNER_H
 
 #include "storage/buf.h"
+#include "storage/fd.h"
 #include "utils/catcache.h"
 #include "utils/rel.h"
 
@@ -106,5 +107,12 @@ extern void ResourceOwnerRememberRelationRef(ResourceOwner owner,
 								 Relation rel);
 extern void ResourceOwnerForgetRelationRef(ResourceOwner owner,
 							   Relation rel);
+
+/* support for temporary file management */
+extern void ResourceOwnerEnlargeFiles(ResourceOwner owner);
+extern void ResourceOwnerRememberFile(ResourceOwner owner,
+						  File file);
+extern void ResourceOwnerForgetFile(ResourceOwner owner,
+						File file);
 
 #endif   /* RESOWNER_H */
