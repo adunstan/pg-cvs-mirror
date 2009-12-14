d94b016af425e931c34d02fa92af6ae131e48bc6
Tom Lane <tgl@postgresql.org>
2009-12-14 02:16:04 +0000

    Fix a bug introduced when set-returning SQL functions were made inline-able: we have to cope with the possibility that the declared result rowtype contains dropped columns.  This fails in 8.4, as per bug #5240.
    While at it, be more paranoid about inserting binary coercions when inlining.
    The pre-8.4 code did not really need to worry about that because it could not
    inline at all in any case where an added coercion could change the behavior
    of the function's statement.  However, when inlining a SRF we allow sorting,
    grouping, and set-ops such as UNION.  In these cases, modifying one of the
    targetlist entries that the sort/group/setop depends on could conceivably
    change the behavior of the function's statement --- so don't inline when
    such a case applies.

--- src/include/executor/functions.h
+++ src/include/executor/functions.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2009, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/executor/functions.h,v 1.32 2008/10/31 19:37:56 tgl Exp $
+ * $PostgreSQL: pgsql/src/include/executor/functions.h,v 1.33 2009/01/01 17:23:59 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -22,7 +22,7 @@ extern Datum fmgr_sql(PG_FUNCTION_ARGS);
 
 extern bool check_sql_fn_retval(Oid func_id, Oid rettype,
 					List *queryTreeList,
-					bool insertRelabels,
+					bool *modifyTargetList,
 					JunkFilter **junkFilter);
 
 extern DestReceiver *CreateSQLFunctionDestReceiver(void);
