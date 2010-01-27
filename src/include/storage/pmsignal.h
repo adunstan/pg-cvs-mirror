1f4f30a6b467b386f30becf26870cb2153650f42
Heikki Linnakangas <heikki@postgresql.org>
2010-01-27 15:27:51 +0000

    Make standby server continuously retry restoring the next WAL segment with restore_command, if the connection to the primary server is lost. This ensures that the standby can recover automatically, if the connection is lost for a long time and standby falls behind so much that the required WAL segments have been archived and deleted in the master.
    This also makes standby_mode useful without streaming replication; the
    server will keep retrying restore_command every few seconds until the
    trigger file is found. That's the same basic functionality pg_standby
    offers, but without the bells and whistles.
    
    To implement that, refactor the ReadRecord/FetchRecord functions. The
    FetchRecord() function introduced in the original streaming replication
    patch is removed, and all the retry logic is now in a new function called
    XLogReadPage(). XLogReadPage() is now responsible for executing
    restore_command, launching walreceiver, and waiting for new WAL to arrive
    from primary, as required.
    
    This also changes the life cycle of walreceiver. When launched, it now only
    tries to connect to the master once, and exits if the connection fails, or
    is lost during streaming for any reason. The startup process detects the
    death, and re-launches walreceiver if necessary.

--- src/include/storage/pmsignal.h
+++ src/include/storage/pmsignal.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2010, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/storage/pmsignal.h,v 1.27 2010/01/02 16:58:08 momjian Exp $
+ * $PostgreSQL: pgsql/src/include/storage/pmsignal.h,v 1.28 2010/01/15 09:19:09 heikki Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -30,7 +30,6 @@ typedef enum
 	PMSIGNAL_START_AUTOVAC_LAUNCHER,	/* start an autovacuum launcher */
 	PMSIGNAL_START_AUTOVAC_WORKER,		/* start an autovacuum worker */
 	PMSIGNAL_START_WALRECEIVER,			/* start a walreceiver */
-	PMSIGNAL_SHUTDOWN_WALRECEIVER,		/* shut down a walreceiver */
 
 	NUM_PMSIGNALS				/* Must be last value of enum! */
 } PMSignalReason;
