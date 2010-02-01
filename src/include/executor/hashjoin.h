c7b9f995f78ba55dc05994bcf1569e8c10bffccb
Robert Haas <rhaas@postgresql.org>
2010-02-01 15:43:36 +0000

    Augment EXPLAIN output with more details on Hash nodes.
    We show the number of buckets, the number of batches (and also the original
    number if it has changed), and the peak space used by the hash table.  Minor
    executor changes to track peak space used.

--- src/include/executor/hashjoin.h
+++ src/include/executor/hashjoin.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2010, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/executor/hashjoin.h,v 1.51 2009/06/11 14:49:11 momjian Exp $
+ * $PostgreSQL: pgsql/src/include/executor/hashjoin.h,v 1.52 2010/01/02 16:58:03 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -149,6 +149,7 @@ typedef struct HashJoinTableData
 
 	Size		spaceUsed;		/* memory space currently used by tuples */
 	Size		spaceAllowed;	/* upper limit for space used */
+	Size		spacePeak;		/* peak space used */
 	Size		spaceUsedSkew;	/* skew hash table's current space usage */
 	Size		spaceAllowedSkew;		/* upper limit for skew hashtable */
 
