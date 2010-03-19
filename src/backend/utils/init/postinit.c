6a7ceabe58b12ba4d32733d35ed04d0e170645ae
Simon Riggs <sriggs@postgresql.org>
2010-03-19 19:19:38 +0000

    Add connection messages for streaming replication. log_connections was broken for a replication connection and no messages were displayed on either standby or primary, at any debug level. Connection messages needed to diagnose session drop/reconnect events. Use LOG mode for now, discuss lowering in later releases.

--- src/backend/utils/init/postinit.c
+++ src/backend/utils/init/postinit.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/utils/init/postinit.c,v 1.203 2010/02/14 18:42:18 rhaas Exp $
+ *	  $PostgreSQL: pgsql/src/backend/utils/init/postinit.c,v 1.204 2010/02/26 02:01:13 momjian Exp $
  *
  *
  *-------------------------------------------------------------------------
@@ -217,7 +217,14 @@ PerformAuthentication(Port *port)
 	if (!disable_sig_alarm(true))
 		elog(FATAL, "could not disable timer for authorization timeout");
 
-	if (Log_connections)
+	/*
+	 * Log connection for streaming replication even if Log_connections disabled.
+	 */
+	if (am_walsender)
+		ereport(LOG,
+				(errmsg("connection authorized: user=%s database=replication",
+						port->user_name)));
+	else if (Log_connections)
 		ereport(LOG,
 				(errmsg("connection authorized: user=%s database=%s",
 						port->user_name, port->database_name)));
