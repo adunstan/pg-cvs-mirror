d79e42c830e6eb5a70372a48b2a6fc950e0ca781
Tom Lane <tgl@postgresql.org>
2010-04-15 21:05:23 +0000

    Fix psql's \copy to not insert spaces around dots and commas in the text of the SELECT query in \copy (SELECT ...) commands.  This is unnecessary and breaks numeric literals, as seen in bug #5411 from Vitalii Tymchyshyn.
    This change has already been made in passing in HEAD; backpatch to 8.2
    through 8.4 (earlier releases don't have COPY (SELECT ...) at all).

--- src/bin/psql/copy.c
+++ src/bin/psql/copy.c
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 2000-2006, PostgreSQL Global Development Group
  *
- * $PostgreSQL: pgsql/src/bin/psql/copy.c,v 1.71.2.1 2007/02/05 15:23:24 adunstan Exp $
+ * $PostgreSQL: pgsql/src/bin/psql/copy.c,v 1.71.2.2 2009/08/07 20:16:29 tgl Exp $
  */
 #include "postgres_fe.h"
 #include "copy.h"
@@ -152,7 +152,7 @@ parse_slash_copy(const char *args)
 
 		while (parens > 0)
 		{
-			token = strtokx(NULL, whitespace, ".,()", "\"'",
+			token = strtokx(NULL, whitespace, "()", "\"'",
 							nonstd_backslash, true, false, pset.encoding);
 			if (!token)
 				goto error;
