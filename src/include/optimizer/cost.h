9992ce129fa471be4f78a058203b06d694f6b3a1
Robert Haas <rhaas@postgresql.org>
2010-04-19 00:55:26 +0000

    Add an 'enable_material' GUC.
    The logic for determining whether to materialize has been significantly
    overhauled for 9.0.  In case there should be any doubt about whether
    materialization is a win in any particular case, this should provide a
    convenient way of seeing what happens without it; but even with enable_material
    turned off, we still materialize in cases where it is required for
    correctness.
    
    Thanks to Tom Lane for the review.

--- src/include/optimizer/cost.h
+++ src/include/optimizer/cost.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2010, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/optimizer/cost.h,v 1.99 2009/11/15 02:45:35 tgl Exp $
+ * $PostgreSQL: pgsql/src/include/optimizer/cost.h,v 1.100 2010/01/02 16:58:07 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -57,6 +57,7 @@ extern bool enable_tidscan;
 extern bool enable_sort;
 extern bool enable_hashagg;
 extern bool enable_nestloop;
+extern bool enable_material;
 extern bool enable_mergejoin;
 extern bool enable_hashjoin;
 extern int	constraint_exclusion;
