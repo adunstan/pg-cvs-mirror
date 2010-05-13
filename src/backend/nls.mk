b2df5f4dbd43a4bf47b0671950dd2fc0abe95cf0
Peter Eisentraut <petere@postgresql.org>
2010-05-13 10:50:20 +0000

    Translation update

--- src/backend/nls.mk
+++ src/backend/nls.mk
@@ -1,6 +1,6 @@
-# $PostgreSQL: pgsql/src/backend/nls.mk,v 1.26 2009/06/04 18:33:07 tgl Exp $
+# $PostgreSQL: pgsql/src/backend/nls.mk,v 1.27 2009/06/26 19:33:43 petere Exp $
 CATALOG_NAME	:= postgres
-AVAIL_LANGUAGES	:= de es fr ja pt_BR tr
+AVAIL_LANGUAGES	:= de es fr ja pt_BR tr zh_CN
 GETTEXT_FILES	:= + gettext-files
 GETTEXT_TRIGGERS:= _ errmsg errmsg_plural:1,2 errdetail errdetail_log errdetail_plural:1,2 errhint errcontext write_stderr yyerror
 
