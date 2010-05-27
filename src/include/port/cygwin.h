24a254da877c3ef41b54f5a514d52ca6ea7676b0
Takahiro Itagaki <itagaki@postgresql.org>
2010-05-27 07:59:48 +0000

    Mark PG_MODULE_MAGIC and PG_FUNCTION_INFO_V1 with PGDLLEXPORT independently from BUILDING_DLL. It is always __declspec(dllexport).

--- src/include/port/cygwin.h
+++ src/include/port/cygwin.h
@@ -1,4 +1,4 @@
-/* $PostgreSQL: pgsql/src/include/port/cygwin.h,v 1.6 2003/12/23 03:31:30 momjian Exp $ */
+/* $PostgreSQL: pgsql/src/include/port/cygwin.h,v 1.7 2007/07/25 12:22:53 mha Exp $ */
 
 #include <cygwin/version.h>
 
@@ -17,5 +17,6 @@
 #define PGDLLIMPORT __declspec (dllexport)
 #else
 #define PGDLLIMPORT __declspec (dllimport)
-
 #endif
+
+#define PGDLLEXPORT __declspec (dllexport)
