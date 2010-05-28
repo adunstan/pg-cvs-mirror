ff539b67280e50915091bc83381ae54d549e20bf
Takahiro Itagaki <itagaki@postgresql.org>
2010-05-28 16:34:15 +0000

    PGDLLEXPORT is __declspec (dllexport) only on MSVC, but is __declspec (dllimport) on other compilers because cygwin and mingw don't like dllexport.

--- src/include/port/cygwin.h
+++ src/include/port/cygwin.h
@@ -1,4 +1,4 @@
-/* $PostgreSQL: pgsql/src/include/port/cygwin.h,v 1.7 2007/07/25 12:22:53 mha Exp $ */
+/* $PostgreSQL: pgsql/src/include/port/cygwin.h,v 1.8 2010/05/27 07:59:48 itagaki Exp $ */
 
 #include <cygwin/version.h>
 
@@ -19,4 +19,4 @@
 #define PGDLLIMPORT __declspec (dllimport)
 #endif
 
-#define PGDLLEXPORT __declspec (dllexport)
+#define PGDLLEXPORT __declspec (dllimport)
--- src/include/port/win32.h
+++ src/include/port/win32.h
@@ -1,4 +1,4 @@
-/* $PostgreSQL: pgsql/src/include/port/win32.h,v 1.93 2010/02/16 19:26:02 mha Exp $ */
+/* $PostgreSQL: pgsql/src/include/port/win32.h,v 1.94 2010/05/27 07:59:48 itagaki Exp $ */
 
 #if defined(_MSC_VER) || defined(__BORLANDC__)
 #define WIN32_ONLY_COMPILER
@@ -58,7 +58,11 @@
 #define PGDLLIMPORT __declspec (dllimport)
 #endif
 
+#ifdef _MSC_VER
 #define PGDLLEXPORT __declspec (dllexport)
+#else
+#define PGDLLEXPORT __declspec (dllimport)
+#endif
 
 #else							/* not CYGWIN, not MSVC, not MingW */
 #define PGDLLIMPORT
