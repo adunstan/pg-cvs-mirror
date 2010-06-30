ef19c86acea4241ad445a94c350431df1db80185
Heikki Linnakangas <heikki@postgresql.org>
2010-06-30 18:11:19 +0000

    stringToNode() and deparse_expression_pretty() crash on invalid input, but we have nevertheless exposed them to users via pg_get_expr(). It would be too much maintenance effort to rigorously check the input, so put a hack in place instead to restrict pg_get_expr() so that the argument must come from one of the system catalog columns known to contain valid expressions.
    Per report from Rushabh Lathia. Backpatch to 7.4 which is the oldest
    supported version at the moment.

--- src/include/catalog/pg_constraint.h
+++ src/include/catalog/pg_constraint.h
@@ -8,7 +8,7 @@
  * Portions Copyright (c) 1996-2005, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/catalog/pg_constraint.h,v 1.18 2005/10/15 02:49:42 momjian Exp $
+ * $PostgreSQL: pgsql/src/include/catalog/pg_constraint.h,v 1.18.2.1 2005/11/22 18:23:27 momjian Exp $
  *
  * NOTES
  *	  the genbki.sh script reads this file and generates .bki
@@ -18,6 +18,8 @@
  */
 #ifndef PG_CONSTRAINT_H
 #define PG_CONSTRAINT_H
+
+#include "nodes/pg_list.h"
 
 /* ----------------
  *		postgres.h contains the system type definitions and the
