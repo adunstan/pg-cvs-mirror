97bd3b3991d07f62823a71d7d30f903b75273c7a
Robert Haas <rhaas@postgresql.org>
2010-07-03 13:53:26 +0000

    Allow REASSIGNED OWNED to handle opclasses and opfamilies.
    Backpatch to 8.3, which is as far back as we have opfamilies.
    The opclass portion could probably be backpatched to 8.2, when
    REASSIGN OWNED was added, but for now I have not done that.
    
    Asko Tiidumaa, with minor adjustments by me.

--- src/backend/catalog/pg_shdepend.c
+++ src/backend/catalog/pg_shdepend.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/catalog/pg_shdepend.c,v 1.33 2009/06/04 18:33:06 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/backend/catalog/pg_shdepend.c,v 1.34 2009/06/11 14:48:55 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -26,6 +26,8 @@
 #include "catalog/pg_language.h"
 #include "catalog/pg_namespace.h"
 #include "catalog/pg_operator.h"
+#include "catalog/pg_opclass.h"
+#include "catalog/pg_opfamily.h"
 #include "catalog/pg_proc.h"
 #include "catalog/pg_shdepend.h"
 #include "catalog/pg_tablespace.h"
@@ -1363,6 +1365,14 @@ shdepReassignOwned(List *roleids, Oid newrole)
 
 				case LanguageRelationId:
 					AlterLanguageOwner_oid(sdepForm->objid, newrole);
+					break;
+
+				case OperatorClassRelationId:
+					AlterOpClassOwner_oid(sdepForm->objid, newrole);
+					break;
+
+				case OperatorFamilyRelationId:
+					AlterOpFamilyOwner_oid(sdepForm->objid, newrole);
 					break;
 
 				default:
