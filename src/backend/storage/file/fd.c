7cf715acbc72886c3967a924810ffc278e20dbb4
Robert Haas <rhaas@postgresql.org>
2010-07-06 22:55:26 +0000

    Make log_temp_files based on kB, and revert docs & comments to match.
    Per extensive discussion on pgsql-hackers.  We are deliberately not
    back-patching this even though the behavior of 8.3 and 8.4 is
    unquestionably broken, for fear of breaking existing users of this
    parameter.  This incompatibility should be release-noted.

--- src/backend/storage/file/fd.c
+++ src/backend/storage/file/fd.c
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1994, Regents of the University of California
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/storage/file/fd.c,v 1.155 2010/02/22 15:26:14 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/backend/storage/file/fd.c,v 1.156 2010/02/26 02:00:59 momjian Exp $
  *
  * NOTES:
  *
@@ -1062,7 +1062,7 @@ FileClose(File file)
 		{
 			if (stat(vfdP->fileName, &filestats) == 0)
 			{
-				if (filestats.st_size >= log_temp_files)
+				if ((filestats.st_size / 1024) >= log_temp_files)
 					ereport(LOG,
 							(errmsg("temporary file: path \"%s\", size %lu",
 									vfdP->fileName,
