896d3aa90b546614e86bf08bfa063999ff02b471
Tom Lane <tgl@postgresql.org>
2010-07-29 23:16:41 +0000

    Improved version of patch to protect pg_get_expr() against misuse: look through join alias Vars to avoid breaking join queries, and move the test to someplace where it will catch more possible ways of calling a function.  We still ought to throw away the whole thing in favor of a data-type-based solution, but that's not feasible in the back branches.
    This needs to be back-patched further than 9.0, but I don't have time
    to do so today.  Committing now so that the fix gets into 9.0beta4.

--- src/include/parser/parse_func.h
+++ src/include/parser/parse_func.h
@@ -7,7 +7,7 @@
  * Portions Copyright (c) 1996-2010, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/parser/parse_func.h,v 1.67 2009/12/15 17:57:47 tgl Exp $
+ * $PostgreSQL: pgsql/src/include/parser/parse_func.h,v 1.68 2010/01/02 16:58:07 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -81,5 +81,7 @@ extern Oid LookupFuncNameTypeNames(List *funcname, List *argtypes,
 						bool noError);
 extern Oid LookupAggNameTypeNames(List *aggname, List *argtypes,
 					   bool noError);
+
+extern void check_pg_get_expr_args(ParseState *pstate, Oid fnoid, List *args);
 
 #endif   /* PARSE_FUNC_H */
