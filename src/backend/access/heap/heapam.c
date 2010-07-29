d48cd86a629c0958a03999a9418cfae3d2d2d0b3
Robert Haas <rhaas@postgresql.org>
2010-07-29 16:15:33 +0000

    Fix possible page corruption by ALTER TABLE .. SET TABLESPACE.
    If a zeroed page is present in the heap, ALTER TABLE .. SET TABLESPACE will
    set the LSN and TLI while copying it, which is wrong, and heap_xlog_newpage()
    will do the same thing during replay, so the corruption propagates to any
    standby.  Note, however, that the bug can't be demonstrated unless archiving
    is enabled, since in that case we skip WAL logging altogether, and the LSN/TLI
    are not set.
    
    Back-patch to 8.0; prior releases do not have tablespaces.
    
    Analysis and patch by Jeff Davis.  Adjustments for back-branches and minor
    wordsmithing by me.

--- src/backend/access/heap/heapam.c
+++ src/backend/access/heap/heapam.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/access/heap/heapam.c,v 1.200.2.2 2005/11/22 18:23:03 momjian Exp $
+ *	  $PostgreSQL: pgsql/src/backend/access/heap/heapam.c,v 1.200.2.3 2006/11/17 18:00:25 tgl Exp $
  *
  *
  * INTERFACE ROUTINES
@@ -2740,8 +2740,16 @@ heap_xlog_newpage(XLogRecPtr lsn, XLogRecord *record)
 	Assert(record->xl_len == SizeOfHeapNewpage + BLCKSZ);
 	memcpy(page, (char *) xlrec + SizeOfHeapNewpage, BLCKSZ);
 
-	PageSetLSN(page, lsn);
-	PageSetTLI(page, ThisTimeLineID);
+	/*
+	 * The page may be uninitialized. If so, we can't set the LSN
+	 * and TLI because that would corrupt the page.
+	 */
+	if (!PageIsNew(page))
+	{
+		PageSetLSN(page, lsn);
+		PageSetTLI(page, ThisTimeLineID);
+	}
+
 	LockBuffer(buffer, BUFFER_LOCK_UNLOCK);
 	WriteBuffer(buffer);
 }
