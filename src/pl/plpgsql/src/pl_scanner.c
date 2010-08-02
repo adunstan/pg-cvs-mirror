cec1425f0a6767d90144fbc34303b4ba2bc6e5e9
Robert Haas <rhaas@postgresql.org>
2010-08-02 03:46:54 +0000

    Remove ancient PL/pgsql line numbering hack.
    While this hack arguably has some benefit in terms of making PL/pgsql's
    line numbering match the programmer's expectations, it also makes
    PL/pgsql inconsistent with the remaining PLs, making it difficult for
    clients to reliably determine where the error actually is.  On balance,
    it seems better to be consistent.
    
    Pavel Stehule

--- src/pl/plpgsql/src/pl_scanner.c
+++ src/pl/plpgsql/src/pl_scanner.c
@@ -9,7 +9,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/pl/plpgsql/src/pl_scanner.c,v 1.4 2010/01/10 17:15:18 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/pl/plpgsql/src/pl_scanner.c,v 1.5 2010/02/26 02:01:35 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -518,19 +518,6 @@ location_lineno_init(void)
 {
 	cur_line_start = scanorig;
 	cur_line_num = 1;
-
-	/*----------
-	 * Hack: skip any initial newline, so that in the common coding layout
-	 *		CREATE FUNCTION ... AS $$
-	 *			code body
-	 *		$$ LANGUAGE plpgsql;
-	 * we will think "line 1" is what the programmer thinks of as line 1.
-	 *----------
-	 */
-	if (*cur_line_start == '\r')
-		cur_line_start++;
-	if (*cur_line_start == '\n')
-		cur_line_start++;
 
 	cur_line_end = strchr(cur_line_start, '\n');
 }
