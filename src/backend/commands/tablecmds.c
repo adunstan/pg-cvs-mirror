5b30a1b32ef4f9f0346df21cd705f09b3b6cf1f0
Robert Haas <rhaas@postgresql.org>
2010-08-03 15:47:02 +0000

    Fix inheritance count tracking in ALTER TABLE .. ADD CONSTRAINT.
    Without this patch, constraints inherited by children of a parent
    table which itself has multiple inheritance parents can end up with
    the wrong coninhcount.  After dropping the constraint, the children
    end up with a leftover copy of the constraint that is not dumped
    and cannot be dropped.  There is a similar problem with ALTER TABLE
    .. ADD COLUMN, but that looks significantly more difficult to
    resolve, so I'm committing this fix separately.
    
    Back-patch to 8.4, which is the first release that has coninhcount.
    
    Report by Hank Enting.

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/commands/tablecmds.c,v 1.336 2010/07/29 11:06:34 sriggs Exp $
+ *	  $PostgreSQL: pgsql/src/backend/commands/tablecmds.c,v 1.337 2010/07/29 19:23:20 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -4889,6 +4889,15 @@ ATAddCheckConstraint(List **wqueue, AlteredTableInfo *tab, Relation rel,
 
 	/* Advance command counter in case same table is visited multiple times */
 	CommandCounterIncrement();
+
+	/*
+	 * If the constraint got merged with an existing constraint, we're done.
+	 * We mustn't recurse to child tables in this case, because they've already
+	 * got the constraint, and visiting them again would lead to an incorrect
+	 * value for coninhcount.
+	 */
+	if (newcons == NIL)
+		return;
 
 	/*
 	 * Propagate to children as appropriate.  Unlike most other ALTER
