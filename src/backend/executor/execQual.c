dd172bceccb5607e2f9e4b526aeb5a0f97b53cb4
Tom Lane <tgl@postgresql.org>
2010-08-26 18:54:52 +0000

    Fix ExecMakeTableFunctionResult to verify that all rows returned by a SRF returning "record" actually do have the same rowtype.  This is needed because the parser can't realistically enforce that they will all have the same typmod, as seen in a recent example from David Wheeler.
    Back-patch to 8.0, which is as far back as we have the notion of RECORD
    subtypes being distinguished by typmod.  Wheeler's example depends on
    8.4-and-up features, but I suspect there may be ways to provoke similar
    failures before 8.4.

--- src/backend/executor/execQual.c
+++ src/backend/executor/execQual.c
@@ -8,7 +8,7 @@
  *
  *
  * IDENTIFICATION
- *	  $PostgreSQL: pgsql/src/backend/executor/execQual.c,v 1.250.2.3 2010/01/11 15:31:12 tgl Exp $
+ *	  $PostgreSQL: pgsql/src/backend/executor/execQual.c,v 1.250.2.4 2010/02/18 18:41:55 tgl Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -2095,6 +2095,16 @@ ExecMakeTableFunctionResult(ExprState *funcexpr,
 				HeapTupleHeader td;
 
 				td = DatumGetHeapTupleHeader(result);
+
+				/*
+				 * Verify all returned rows have same subtype; necessary in
+				 * case the type is RECORD.
+				 */
+				if (HeapTupleHeaderGetTypeId(td) != tupdesc->tdtypeid ||
+					HeapTupleHeaderGetTypMod(td) != tupdesc->tdtypmod)
+					ereport(ERROR,
+							(errcode(ERRCODE_DATATYPE_MISMATCH),
+							 errmsg("rows returned by function are not all of the same row type")));
 
 				/*
 				 * tuplestore_puttuple needs a HeapTuple not a bare
