51b0bd50a20bf76d4ab0e4ffecb6689a8a9f459b
Tom Lane <tgl@postgresql.org>
2010-09-02 03:17:06 +0000

    Fix up flushing of composite-type typcache entries to be driven directly by SI invalidation events, rather than indirectly through the relcache.
    In the previous coding, we had to flush a composite-type typcache entry
    whenever we discarded the corresponding relcache entry.  This caused problems
    at least when testing with RELCACHE_FORCE_RELEASE, as shown in recent report
    from Jeff Davis, and might result in real-world problems given the kind of
    unexpected relcache flush that that test mechanism is intended to model.
    
    The new coding decouples relcache and typcache management, which is a good
    thing anyway from a structural perspective.  The cost is that we have to
    search the typcache linearly to find entries that need to be flushed.  There
    are a couple of ways we could avoid that, but at the moment it's not clear
    it's worth any extra trouble, because the typcache contains very few entries
    in typical operation.
    
    Back-patch to 8.2, the same as some other recent fixes in this general area.
    The patch could be carried back to 8.0 with some additional work, but given
    that it's only hypothetical whether we're fixing any problem observable in
    the field, it doesn't seem worth the work now.

--- src/include/utils/typcache.h
+++ src/include/utils/typcache.h
@@ -9,7 +9,7 @@
  * Portions Copyright (c) 1996-2008, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
- * $PostgreSQL: pgsql/src/include/utils/typcache.h,v 1.15 2007/11/15 21:14:45 momjian Exp $
+ * $PostgreSQL: pgsql/src/include/utils/typcache.h,v 1.16 2008/01/01 19:45:59 momjian Exp $
  *
  *-------------------------------------------------------------------------
  */
@@ -85,7 +85,5 @@ extern TupleDesc lookup_rowtype_tupdesc_noerror(Oid type_id, int32 typmod,
 extern TupleDesc lookup_rowtype_tupdesc_copy(Oid type_id, int32 typmod);
 
 extern void assign_record_type_typmod(TupleDesc tupDesc);
-
-extern void flush_rowtype_cache(Oid type_id);
 
 #endif   /* TYPCACHE_H */
